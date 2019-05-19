d9ac7543560d36d66a78854ddbc682e65d38de2c
sugyan <sugi1982@gmail.com>
2019-05-19 13:33:05 +0900

    Add 437

---
+++ problems/0437-path-sum-iii/main.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        return pathSum(root, sum, false);
+    }
+private:
+    int pathSum(TreeNode* root, int sum, bool include) {
+        if (root == nullptr) return 0;
+        int answer = 0;
+        if (root->val == sum) ++answer;
+        if (root->left != nullptr) {
+            answer += pathSum(root->left, sum - root->val, true);
+            if (!include) {
+                answer += pathSum(root->left, sum, false);
+            }
+        }
+        if (root->right != nullptr) {
+            answer += pathSum(root->right, sum - root->val, true);
+            if (!include) {
+                answer += pathSum(root->right, sum, false);
+            }
+        }
+        return answer;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        getline(cin, line);
+        int sum = stringToInteger(line);
+        
+        int ret = Solution().pathSum(root, sum);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
