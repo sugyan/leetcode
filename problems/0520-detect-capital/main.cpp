c261d6c4b85dd2097be369b34800e83d88d26dc8
sugyan <sugi1982@gmail.com>
2019-06-02 19:12:54 +0900

    Add 520

---
+++ problems/0520-detect-capital/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        bool upper = isupper(word[0]);
+        for (int i = 1, length = word.length(); i < length; ++i) {
+            if (!upper && isupper(word[i])) return false;
+            if (isupper(word[i]) != isupper(word[1])) return false;
+        }
+        return true;
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string word = stringToString(line);
+        
+        bool ret = Solution().detectCapitalUse(word);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
