239a3ace006d58faab9cb2feddce52a648e5478c
sugyan <sugi1982@gmail.com>
2019-06-20 19:50:52 +0900

    Add 606

---
+++ problems/0606-construct-string-from-binary-tree/main.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if (t == nullptr) return "";
+        ostringstream answer;
+        dfs(t, answer);
+        return answer.str();
+    }
+private:
+    void dfs(TreeNode* t, ostringstream& answer) {
+        answer << t->val;
+        if (t->left != nullptr) {
+            answer << '(';
+            dfs(t->left, answer);
+            answer << ')';
+        }
+        if (t->right != nullptr) {
+            if (t->left == nullptr) answer << "()";
+            answer << '(';
+            dfs(t->right, answer);
+            answer << ')';
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* t = stringToTreeNode(line);
+        
+        string ret = Solution().tree2str(t);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
