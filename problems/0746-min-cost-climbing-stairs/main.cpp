3f32209d84db592a38dc572ecc1d31860b747926
sugyan <sugi1982@gmail.com>
2019-07-16 23:33:25 +0900

    Add 746

---
+++ problems/0746-min-cost-climbing-stairs/main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        for (int i = 2, size = cost.size(); i < size; ++i) {
+            cost[i] += min(cost[i - 1], cost[i - 2]);
+        }
+        return min(cost[cost.size() - 1], cost[cost.size() - 2]);
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> cost = stringToIntegerVector(line);
+        
+        int ret = Solution().minCostClimbingStairs(cost);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
