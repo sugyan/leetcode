f72e3ab67c1b590f79f060435211ae4d1b768815
sugyan <sugi1982@gmail.com>
2019-08-17 22:43:22 +0900

    Add 872

---
+++ problems/0872-leaf-similar-trees/main.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1, v2;
+        dfs(root1, v1);
+        dfs(root2, v2);
+        cout << v1[0] << endl;
+        cout << v2[0] << endl;
+        return v1 == v2;
+    }
+private:
+    void dfs(TreeNode* node, vector<int>& v) {
+        if (node == nullptr) return;
+        if (node->left != nullptr || node->right != nullptr) {
+            dfs(node->left, v);
+            dfs(node->right, v);
+        } else {
+            v.push_back(node->val);
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root1 = stringToTreeNode(line);
+        getline(cin, line);
+        TreeNode* root2 = stringToTreeNode(line);
+        
+        bool ret = Solution().leafSimilar(root1, root2);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
