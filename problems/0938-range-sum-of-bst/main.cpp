e6876fe515b8ef4bf14b5c40e08e191a8a57da55
sugyan <sugi1982@gmail.com>
2019-09-08 15:21:54 +0900

    Add 938

---
+++ problems/0938-range-sum-of-bst/main.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        int answer = 0;
+        dfs(root, L, R, answer);
+        return answer;
+    }
+private:
+    void dfs(TreeNode* node, int& L, int& R, int& answer) {
+        if (node == nullptr) return;
+        int val = node->val;
+        if (val >= L) {
+            dfs(node->left, L, R, answer);
+        }
+        if (L <= val && val <= R) answer += val;
+        if (val <= R) {
+            dfs(node->right, L, R, answer);
+        }
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        getline(cin, line);
+        int L = stringToInteger(line);
+        getline(cin, line);
+        int R = stringToInteger(line);
+        
+        int ret = Solution().rangeSumBST(root, L, R);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
