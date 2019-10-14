f9758325989e0a6aa6d9a8052e2858d792d745bd
sugyan <sugi1982@gmail.com>
2019-10-14 23:38:26 +0900

    Add 1046

---
+++ problems/1046-last-stone-weight/main.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q(stones.begin(), stones.end());
+        while (q.size() > 1) {
+            int y = q.top();
+            q.pop();
+            int x = q.top();
+            q.pop();
+            if (y - x > 0) q.push(y - x);
+        }
+        if (q.empty()) return 0;
+        return q.top();
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> stones = stringToIntegerVector(line);
+        
+        int ret = Solution().lastStoneWeight(stones);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
