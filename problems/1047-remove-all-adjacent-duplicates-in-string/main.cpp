0f80525d15b5ccea2dbdcaf3a2b327db0f6b40a9
sugyan <sugi1982@gmail.com>
2019-10-15 23:19:48 +0900

    Add 1047

---
+++ problems/1047-remove-all-adjacent-duplicates-in-string/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        vector<char> v;
+        for (char& c : S) {
+            if (!v.empty() && v.back() == c) {
+                v.pop_back();
+            } else {
+                v.push_back(c);
+            }
+        }
+        return string(v.begin(), v.end());
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string S = stringToString(line);
+        
+        string ret = Solution().removeDuplicates(S);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
