0c3fa2efbccc480b57a68ca5cdd6e1c3933566f9
sugyan <sugi1982@gmail.com>
2019-11-07 23:50:06 +0900

    Add 9

---
+++ problems/0009-palindrome-number/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x > 0)) return false;
+        int r = 0;
+        while (x > r) {
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+        return x == r || x == r / 10;
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int x = stringToInteger(line);
+
+        bool ret = Solution().isPalindrome(x);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
