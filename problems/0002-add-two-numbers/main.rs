77c553c91109d234c96cddbd74df237b1744dd66
sugyan <sugi1982@gmail.com>
2019-11-14 19:36:01 +0900

    Use external crate

---
+++ problems/0002-add-two-numbers/main.rs
@@ -0,0 +1,16 @@
+use utils::ListNode;
+
+struct Solution {}
+
+impl Solution {
+  pub fn add_two_numbers(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+  ) -> Option<Box<ListNode>> {
+    return l1;
+  }
+}
+
+fn main() {
+  println!("{:?}", ListNode::new(3));
+}
---
+++ utils/src/lib.rs
@@ -0,0 +1,12 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
