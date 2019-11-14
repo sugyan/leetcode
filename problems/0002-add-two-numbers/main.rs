56b794441cdc85b3e5182bfd55cd5ff0f2597258
sugyan <sugi1982@gmail.com>
2019-11-15 00:54:12 +0900

    Add 2

--- problems/0002-add-two-numbers/main.rs
+++ problems/0002-add-two-numbers/main.rs
@@ -1,16 +1,60 @@
 use utils::ListNode;
 
-struct Solution {}
+pub struct Solution {}
 
 impl Solution {
-  pub fn add_two_numbers(
-    l1: Option<Box<ListNode>>,
-    l2: Option<Box<ListNode>>,
-  ) -> Option<Box<ListNode>> {
-    return l1;
-  }
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let (mut l1, mut l2) = (l1, l2);
+        let mut answer = Some(Box::new(ListNode::new(0)));
+        let mut tail = &mut answer;
+        let mut carry = false;
+        loop {
+            if !carry && l1.is_none() && l2.is_none() {
+                break;
+            }
+            let mut d = 0;
+            d += if let Some(l) = l1 {
+                l1 = l.next;
+                l.val
+            } else {
+                0
+            };
+            d += if let Some(l) = l2 {
+                l2 = l.next;
+                l.val
+            } else {
+                0
+            };
+            d += if carry { 1 } else { 0 };
+            carry = d >= 10;
+            if carry {
+                d -= 10;
+            }
+            if let Some(t) = tail {
+                t.next = Some(Box::new(ListNode::new(d)));
+                tail = &mut t.next;
+            }
+        }
+        return answer.unwrap().next;
+    }
 }
 
 fn main() {
-  println!("{:?}", ListNode::new(3));
+    // TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utils::to_list;
+
+    #[test]
+    fn example_1() {
+        let l1: Option<Box<ListNode>> = to_list(vec![2, 4, 3]);
+        let l2: Option<Box<ListNode>> = to_list(vec![5, 6, 4]);
+        assert_eq!(to_list(vec![7, 0, 8]), Solution::add_two_numbers(l1, l2));
+    }
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -10,3 +10,21 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+
+pub fn to_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in v.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
