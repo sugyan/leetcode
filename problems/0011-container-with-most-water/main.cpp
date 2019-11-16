1c4d8b5534d780ef50fe0ea6f1c47107095f3ea1
sugyan <sugi1982@gmail.com>
2019-11-16 17:32:56 +0900

    Add 11

--- problems/0011-container-with-most-water/main.cpp
+++ problems/0011-container-with-most-water/main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 class Solution {
-public:
-    int maxArea(vector<int>& height) {
+   public:
+    int maxArea(vector<int> &height) {
         int l = 0, r = height.size() - 1;
         int lmax = height[l], rmax = height[r];
         int answer = min(lmax, rmax) * r;
@@ -23,15 +23,15 @@ public:
 };
 
 void trimLeftTrailingSpaces(string &input) {
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
+    input.erase(input.begin(), find_if(input.begin(), input.end(),
+                                       [](int ch) { return !isspace(ch); }));
 }
 
 void trimRightTrailingSpaces(string &input) {
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
+    input.erase(find_if(input.rbegin(), input.rend(),
+                        [](int ch) { return !isspace(ch); })
+                    .base(),
+                input.end());
 }
 
 vector<int> stringToIntegerVector(string input) {
@@ -53,7 +53,7 @@ int main() {
     string line;
     while (getline(cin, line)) {
         vector<int> height = stringToIntegerVector(line);
-        
+
         int ret = Solution().maxArea(height);
 
         string out = to_string(ret);
---
+++ problems/0011-container-with-most-water/main.rs
@@ -0,0 +1,33 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let (mut l, mut r) = (0, height.len() - 1);
+        let mut answer = std::cmp::min(height[0], height[r]) * (r - l) as i32;
+        while l < r {
+            if height[l] < height[r] {
+                let h = height[l];
+                while height[l] <= h && l < r {
+                    l += 1;
+                }
+            } else {
+                let h = height[r];
+                while height[r] <= h && l < r {
+                    r -= 1;
+                }
+            }
+            answer = std::cmp::max(answer, std::cmp::min(height[l], height[r]) * (r - l) as i32);
+        }
+        return answer;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(49, Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]));
+    }
+}
