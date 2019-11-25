9ea5fb81658c54fcb9723824e812f04f42218aa1
sugyan <sugi1982@gmail.com>
2019-11-25 23:59:13 +0900

    Add 39

--- problems/0039-combination-sum/main.cpp
+++ problems/0039-combination-sum/main.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 
 class Solution {
-public:
+   public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> answer;
         vector<int> combination;
         find(candidates, target, answer, combination);
         return answer;
     }
-private:
-    void find(vector<int>& candidates, int target, vector<vector<int>>& answer, vector<int>& combination) {
+
+   private:
+    void find(vector<int>& candidates, int target, vector<vector<int>>& answer,
+              vector<int>& combination) {
         if (target == 0) answer.push_back(combination);
         for (int candidate : candidates) {
-            if (!combination.empty() && candidate < combination.back()) continue;
+            if (!combination.empty() && candidate < combination.back())
+                continue;
             if (candidate <= target) {
                 combination.push_back(candidate);
                 find(candidates, target - candidate, answer, combination);
@@ -25,22 +28,24 @@ private:
 };
 
 int main() {
-    {
-        vector<int> candidates { 2, 3, 6, 7 };
-        int target = 7;
-        vector<vector<int>> ret = Solution().combinationSum(candidates, target);
-        for (vector<int> out : ret) {
-            for (int num : out) cout << num << " ";
-            cout << endl;
-        }
-    }
-    {
-        vector<int> candidates { 2, 3, 5 };
-        int target = 8;
-        vector<vector<int>> ret = Solution().combinationSum(candidates, target);
-        for (vector<int> out : ret) {
-            for (int num : out) cout << num << " ";
-            cout << endl;
-        }
+    vector<pair<vector<int>, int>> inputs{
+        {{2, 3, 6, 7}, 7},
+        {{2, 3, 5}, 8},
+    };
+    for (auto input : inputs) {
+        vector<vector<int>> ret =
+            Solution().combinationSum(input.first, input.second);
+        vector<string> out;
+        transform(ret.begin(), ret.end(), back_inserter(out),
+                  [](vector<int> v) {
+                      ostringstream oss;
+                      for (auto it = v.begin(); it != v.end(); ++it) {
+                          oss << *it;
+                          if (it + 1 != v.end()) oss << ", ";
+                      }
+                      return "[" + oss.str() + "]";
+                  });
+        copy(out.begin(), out.end(), ostream_iterator<string>(cout, " "));
+        cout << endl;
     }
 }
---
+++ problems/0039-combination-sum/main.rs
@@ -0,0 +1,51 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut answer: Vec<Vec<i32>> = Vec::new();
+        let mut v = Vec::new();
+        Solution::helper(&candidates, target, &mut answer, &mut v);
+        return answer;
+    }
+    fn helper(candidates: &Vec<i32>, target: i32, answer: &mut Vec<Vec<i32>>, v: &mut Vec<i32>) {
+        if target == 0 {
+            answer.push(v.clone());
+            return;
+        }
+        for num in candidates {
+            if let Some(last) = v.last() {
+                if num < last {
+                    continue;
+                }
+            }
+            if target >= *num {
+                v.push(*num);
+                Solution::helper(candidates, target - num, answer, v);
+                v.pop();
+            }
+        }
+    }
+}
+
+fn main() {
+    // TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut ret = Solution::combination_sum(vec![2, 3, 6, 7], 7);
+        ret.sort();
+        assert_eq!(vec![vec![2, 2, 3], vec![7]], ret);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut ret = Solution::combination_sum(vec![2, 3, 5], 8);
+        ret.sort();
+        assert_eq!(vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]], ret);
+    }
+}
