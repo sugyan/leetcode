cdb1e09fecf65bb2c071ed58190e182f48b55a44
sugyan <sugi1982@gmail.com>
2019-11-27 23:01:07 +0900

    Add 46

---
+++ problems/0046-permutations/main.rs
@@ -0,0 +1,51 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut answer = Vec::new();
+        let mut v = Vec::new();
+        Solution::helper(&nums, &mut v, &mut answer);
+        return answer;
+    }
+
+    fn helper(nums: &Vec<i32>, v: &mut Vec<i32>, answer: &mut Vec<Vec<i32>>) {
+        if v.len() == nums.len() {
+            answer.push(v.clone());
+        } else {
+            for num in nums {
+                if !v.contains(&num) {
+                    v.push(*num);
+                    Solution::helper(nums, v, answer);
+                    v.pop();
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let v = utils::readline_to_i32_vec();
+    println!("{:?}", Solution::permute(v));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut ret = Solution::permute(vec![1, 2, 3]);
+        ret.sort();
+        assert_eq!(
+            vec![
+                vec![1, 2, 3],
+                vec![1, 3, 2],
+                vec![2, 1, 3],
+                vec![2, 3, 1],
+                vec![3, 1, 2],
+                vec![3, 2, 1]
+            ],
+            ret
+        );
+    }
+}
