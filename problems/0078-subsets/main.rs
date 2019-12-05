b63c3b536ff1859653444500414cbd2c09ba71f6
sugyan <sugi1982@gmail.com>
2019-12-05 22:43:04 +0900

    Add 78

---
+++ problems/0078-subsets/main.rs
@@ -0,0 +1,47 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut answer = Vec::new();
+        answer.push(vec![]);
+        for num in nums {
+            for i in 0..answer.len() {
+                let mut v = answer[i].clone();
+                v.push(num);
+                answer.push(v);
+            }
+        }
+        return answer;
+    }
+}
+
+fn main() {
+    // TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut ret = Solution::subsets(vec![1, 2, 3]);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        assert_eq!(
+            vec![
+                vec![],
+                vec![1],
+                vec![1, 2],
+                vec![1, 2, 3],
+                vec![1, 3],
+                vec![2],
+                vec![2, 3],
+                vec![3]
+            ],
+            ret
+        );
+    }
+}
