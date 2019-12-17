b0611dfe863bba6ca26075a0d36eb4488471110a
sugyan <sugi1982@gmail.com>
2019-12-18 02:11:31 +0900

    Add 73
    https://leetcode.com/problems/set-matrix-zeroes/

---
+++ problems/0073-set-matrix-zeroes/lib.rs
@@ -0,0 +1,58 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
+        let (m, n) = (matrix.len(), matrix[0].len());
+        let mut first_col = false;
+        if (0..m).find(|i| matrix[*i][0] == 0).is_some() {
+            first_col = true;
+        }
+        for i in 0..m {
+            for j in 1..n {
+                if matrix[i][j] == 0 {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for i in 1..m {
+            for j in 1..n {
+                if matrix[i][0] == 0 || matrix[0][j] == 0 {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if matrix[0][0] == 0 {
+            for j in 1..n {
+                matrix[0][j] = 0;
+            }
+        }
+        if first_col {
+            for i in 0..m {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut matrix = vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]];
+        Solution::set_zeroes(&mut matrix);
+        assert_eq!(vec![vec![1, 0, 1], vec![0, 0, 0], vec![1, 0, 1]], matrix);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut matrix = vec![vec![0, 1, 2, 0], vec![3, 4, 5, 2], vec![1, 3, 1, 5]];
+        Solution::set_zeroes(&mut matrix);
+        assert_eq!(
+            vec![vec![0, 0, 0, 0], vec![0, 4, 5, 0], vec![0, 3, 1, 0]],
+            matrix
+        );
+    }
+}
