25089bf435771e2a249f57150471b780fc556cc3
sugyan <sugi1982@gmail.com>
2019-12-19 23:09:58 +0900

    Improve 77 (28ms -> 8ms)

--- problems/0077-combinations/lib.rs
+++ problems/0077-combinations/lib.rs
@@ -4,22 +4,20 @@ impl Solution {
     pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
         let mut answer = Vec::new();
         let mut v = Vec::new();
-        Solution::dfs(&mut answer, n, k, &mut v);
+        Solution::dfs(&mut answer, n, k, 0, &mut v);
         return answer;
     }
-    fn dfs(answer: &mut Vec<Vec<i32>>, n: i32, k: i32, v: &mut Vec<i32>) {
+    fn dfs(answer: &mut Vec<Vec<i32>>, n: i32, k: i32, j: i32, v: &mut Vec<i32>) {
         if v.len() == k as usize {
             answer.push(v.clone());
             return;
         }
-        for i in 0..n {
-            if let Some(last) = v.last() {
-                if *last >= i + 1 {
-                    continue;
-                }
+        for i in j..n {
+            if n - i < k - v.len() as i32 {
+                break;
             }
             v.push(i + 1);
-            Solution::dfs(answer, n, k, v);
+            Solution::dfs(answer, n, k, i + 1, v);
             v.pop();
         }
     }
