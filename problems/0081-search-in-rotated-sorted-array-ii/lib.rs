f40d9b2a2dd79e5dd4dfd2e8132c4279656b20d2
sugyan <sugi1982@gmail.com>
2019-12-21 23:35:06 +0900

    Add 81
    https://leetcode.com/problems/search-in-rotated-sorted-array-ii/

---
+++ problems/0081-search-in-rotated-sorted-array-ii/lib.rs
@@ -0,0 +1,60 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> bool {
+        if nums.is_empty() {
+            return false;
+        }
+        let (mut l, mut r) = (0, nums.len() - 1);
+        while l <= r {
+            let m = l + (r - l) / 2;
+            let n = nums[m];
+            if n == target {
+                return true;
+            }
+            if n > nums[r] {
+                if target > n || target <= nums[r] {
+                    l = m + 1;
+                } else {
+                    if m > 0 {
+                        r = m - 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else if n == nums[r] {
+                if r > 0 {
+                    r -= 1;
+                } else {
+                    break;
+                }
+            } else {
+                if target <= nums[r] && target > n {
+                    l = m + 1;
+                } else {
+                    if m > 0 {
+                        r = m - 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(true, Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 0));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(false, Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 3));
+    }
+}
