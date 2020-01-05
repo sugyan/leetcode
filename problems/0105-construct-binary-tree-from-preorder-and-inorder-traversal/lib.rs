9e4d5487db107f165db0e045b80d87953f863a75
sugyan <sugi1982@gmail.com>
2020-01-05 18:54:12 +0900

    Improve 105 (4ms -> 0ms, 38.2MB -> 2.7MB)

--- problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/lib.rs
+++ problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/lib.rs
@@ -6,21 +6,19 @@ pub struct Solution {}
 
 impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Solution::helper(&preorder, &inorder)
+    }
+    fn helper(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
         if let Some(first) = preorder.first() {
             let root = Rc::new(RefCell::new(TreeNode::new(*first)));
             if let Some(idx) = inorder.iter().position(|v| *v == *first) {
-                root.borrow_mut().left = Solution::build_tree(
-                    preorder.get(1..=idx).unwrap().to_vec(),
-                    inorder.get(0..idx).unwrap().to_vec(),
-                );
-                root.borrow_mut().right = Solution::build_tree(
-                    preorder.get(idx + 1..).unwrap().to_vec(),
-                    inorder.get(idx + 1..).unwrap().to_vec(),
-                );
+                root.borrow_mut().left = Solution::helper(&preorder[1..=idx], &inorder[0..idx]);
+                root.borrow_mut().right =
+                    Solution::helper(&preorder[idx + 1..], &inorder[idx + 1..]);
             }
             return Some(root);
         }
-        // None
+        None
     }
 }
 
