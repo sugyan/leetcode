32008ea81f6bacca50893aa04dc2075ac5f4c303
sugyan <sugi1982@gmail.com>
2020-01-07 15:53:47 +0900

    Improve 0109 (O(N) -> O(logN) space)

--- problems/0109-convert-sorted-list-to-binary-search-tree/lib.rs
+++ problems/0109-convert-sorted-list-to-binary-search-tree/lib.rs
@@ -6,24 +6,28 @@ pub struct Solution {}
 
 impl Solution {
     pub fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
-        let mut head = head;
-        let mut node = &mut head;
-        let mut v: Vec<i32> = Vec::new();
+        let mut node = &head;
+        let mut size = 0;
         while let Some(n) = node {
-            v.push(n.val);
-            node = &mut n.next;
+            size += 1;
+            node = &n.next;
         }
-        Solution::make_bst(&v)
+        Solution::helper(&mut &head, 0, size - 1)
     }
-    fn make_bst(v: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-        if v.is_empty() {
+    fn helper(list: &mut &Option<Box<ListNode>>, l: i32, r: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if l > r {
             return None;
         }
-        let m = v.len() / 2;
-        let node = Rc::new(RefCell::new(TreeNode::new(v[m])));
-        node.borrow_mut().left = Solution::make_bst(&v[0..m]);
-        node.borrow_mut().right = Solution::make_bst(&v[m + 1..]);
-        Some(node)
+        let m = (l + r) / 2;
+        let left = Solution::helper(list, l, m - 1);
+        if let Some(n) = list {
+            let root = Rc::new(RefCell::new(TreeNode::new(n.val)));
+            root.borrow_mut().left = left;
+            *list = &n.next;
+            root.borrow_mut().right = Solution::helper(list, m + 1, r);
+            return Some(root);
+        }
+        None
     }
 }
 
@@ -35,7 +39,15 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(
-            to_tree(vec![Some(0), Some(-3), Some(9), Some(-10), None, Some(5)]),
+            to_tree(vec![
+                Some(0),
+                Some(-10),
+                Some(5),
+                None,
+                Some(-3),
+                None,
+                Some(9)
+            ]),
             Solution::sorted_list_to_bst(to_list(vec![-10, -3, 0, 5, 9]))
         );
     }
