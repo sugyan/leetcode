1661ce43e445f72a848233c6d51cbe2eab1325d7
sugyan <sugi1982@gmail.com>
2020-01-15 22:59:45 +0900

    Improve 131 (backtrack, chars() once)

--- problems/0131-palindrome-partitioning/lib.rs
+++ problems/0131-palindrome-partitioning/lib.rs
@@ -1,38 +1,25 @@
-use std::collections::HashMap;
-
 pub struct Solution {}
 
 impl Solution {
     pub fn partition(s: String) -> Vec<Vec<String>> {
-        let mut hm: HashMap<String, Vec<Vec<String>>> = HashMap::new();
-        Solution::helper(&mut hm, &s);
-        hm.get(&s).unwrap().clone()
+        let mut answer: Vec<Vec<String>> = Vec::new();
+        let mut v: Vec<String> = Vec::new();
+        let s: Vec<char> = s.chars().collect();
+        Solution::helper(&mut answer, &mut v, &s);
+        answer
     }
-    fn helper(hm: &mut HashMap<String, Vec<Vec<String>>>, s: &str) -> Vec<Vec<String>> {
-        if let Some(ret) = hm.get(&s.to_string()) {
-            return ret.clone();
-        }
-        let mut ret: Vec<Vec<String>> = Vec::new();
-        if s.len() == 1 {
-            hm.insert(s.to_string(), vec![vec![s.to_string()]]);
-            return vec![vec![s.to_string()]];
+    fn helper(answer: &mut Vec<Vec<String>>, v: &mut Vec<String>, s: &[char]) {
+        if s.is_empty() {
+            answer.push(v.clone());
+            return;
         }
         for i in 1..=s.len() {
-            let c: Vec<char> = s[0..i].chars().collect();
-            if (0..i / 2).all(|j| c[j] == c[i - 1 - j]) {
-                if i == s.len() {
-                    ret.push(vec![s[0..i].to_string()])
-                } else {
-                    for a in Solution::helper(hm, &s[i..]) {
-                        let mut v: Vec<String> = vec![s[0..i].to_string()];
-                        v.extend(a.into_iter());
-                        ret.push(v);
-                    }
-                }
+            if (0..i / 2).all(|j| s[j] == s[i - 1 - j]) {
+                v.push(s[0..i].iter().collect());
+                Solution::helper(answer, v, &s[i..]);
+                v.pop();
             }
         }
-        hm.insert(s.to_string(), ret.clone());
-        ret
     }
 }
 
