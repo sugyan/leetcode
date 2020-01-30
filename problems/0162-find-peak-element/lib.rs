d8964d1a1ce8432813ed6acdac372b7a21680d56
sugyan <sugi1982@gmail.com>
2020-01-30 22:01:54 +0900

    Add 162
    https://leetcode.com/problems/find-peak-element/

---
+++ problems/0162-find-peak-element/lib.rs
@@ -0,0 +1,27 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        for i in 0..nums.len() {
+            if (i == 0 || nums[i] > nums[i - 1]) && (i == nums.len() - 1 || nums[i] > nums[i + 1]) {
+                return i as i32;
+            }
+        }
+        -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(2, Solution::find_peak_element(vec![1, 2, 3, 1]));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(1, Solution::find_peak_element(vec![1, 2, 1, 3, 5, 6, 4]));
+    }
+}
