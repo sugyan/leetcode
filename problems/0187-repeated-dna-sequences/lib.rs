b4784268f8f28adf82dcc411a929fb5831b328bf
sugyan <sugi1982@gmail.com>
2020-02-05 22:37:40 +0900

    Improve 187 (7MB -> 5.5MB memory)

--- problems/0187-repeated-dna-sequences/lib.rs
+++ problems/0187-repeated-dna-sequences/lib.rs
@@ -4,20 +4,41 @@ pub struct Solution {}
 
 impl Solution {
     pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
-        let mut hm: HashMap<&str, usize> = HashMap::new();
+        let mut hm: HashMap<u32, usize> = HashMap::new();
         if s.len() > 10 {
-            for i in 0..=s.len() - 10 {
-                let s = &s[i..i + 10];
-                if let Some(n) = hm.get_mut(&s) {
+            let mut k = 0;
+            for (i, c) in (0..).zip(s.chars()) {
+                k >>= 2;
+                k += match c {
+                    'A' => 0b00,
+                    'C' => 0b01,
+                    'G' => 0b10,
+                    'T' => 0b11,
+                    _ => std::u32::MAX,
+                } << 18;
+                if i < 9 {
+                    continue;
+                }
+                if let Some(n) = hm.get_mut(&k) {
                     *n += 1;
                 } else {
-                    hm.insert(s, 1);
+                    hm.insert(k, 1);
                 }
             }
         }
         hm.iter()
             .filter(|e| *e.1 > 1)
-            .map(|e| (*e.0).to_string())
+            .map(|e| {
+                (0..10)
+                    .map(|i| match (*e.0 >> (i * 2)) & 0b11 {
+                        0b00 => 'A',
+                        0b01 => 'C',
+                        0b10 => 'G',
+                        0b11 => 'T',
+                        _ => ' ',
+                    })
+                    .collect()
+            })
             .collect()
     }
 }
@@ -28,9 +49,9 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(
-            vec!["AAAAACCCCC", "CCCCCAAAAA"],
-            Solution::find_repeated_dna_sequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".to_string())
-        );
+        let mut ret =
+            Solution::find_repeated_dna_sequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".to_string());
+        ret.sort();
+        assert_eq!(vec!["AAAAACCCCC", "CCCCCAAAAA"], ret);
     }
 }
