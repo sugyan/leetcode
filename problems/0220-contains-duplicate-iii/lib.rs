7ad6c7016867f682946b9b302fea4219c045a494
sugyan <sugi1982@gmail.com>
2020-02-18 09:58:17 +0900

    Improve 220 (116ms -> 4ms)

--- problems/0220-contains-duplicate-iii/lib.rs
+++ problems/0220-contains-duplicate-iii/lib.rs
@@ -1,16 +1,27 @@
+use std::collections::BTreeSet;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn contains_nearby_almost_duplicate(nums: Vec<i32>, k: i32, t: i32) -> bool {
+        if t < 0 {
+            return false;
+        }
+        let k = k as usize;
+        let t = t as i64;
+        let mut bts: BTreeSet<i64> = BTreeSet::new();
         for i in 0..nums.len() {
-            for j in 0..k as usize {
-                if i + 1 + j >= nums.len() {
-                    continue;
-                }
-                if (nums[i] as i64 - nums[i + 1 + j] as i64).abs() <= t as i64 {
-                    return true;
-                }
+            if i > k as usize {
+                bts.remove(&(nums[i - 1 - k] as i64));
             }
+            if bts
+                .range(nums[i] as i64 - t..=nums[i] as i64 + t)
+                .next()
+                .is_some()
+            {
+                return true;
+            }
+            bts.insert(nums[i] as i64);
         }
         false
     }
@@ -42,21 +53,5 @@ mod tests {
             false,
             Solution::contains_nearby_almost_duplicate(vec![1, 5, 9, 1, 5, 9], 2, 3)
         );
-    }
-
-    #[test]
-    fn wa() {
-        assert_eq!(
-            true,
-            Solution::contains_nearby_almost_duplicate(vec![7, 1, 3], 2, 3)
-        );
-        assert_eq!(
-            false,
-            Solution::contains_nearby_almost_duplicate(vec![], 0, 0)
-        );
-        assert_eq!(
-            false,
-            Solution::contains_nearby_almost_duplicate(vec![-1, 2_147_483_647], 1, 2_147_483_647)
-        )
     }
 }
