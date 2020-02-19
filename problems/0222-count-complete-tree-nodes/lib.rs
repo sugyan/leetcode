f778b21e66588e9e0a4840878da98bb013e3076d
sugyan <sugi1982@gmail.com>
2020-02-19 23:01:11 +0900

    Update 222 (recursive solution)

--- problems/0222-count-complete-tree-nodes/lib.rs
+++ problems/0222-count-complete-tree-nodes/lib.rs
@@ -6,11 +6,31 @@ pub struct Solution {}
 
 impl Solution {
     pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        Solution::traverse(&root)
-    }
-    fn traverse(node: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if let Some(n) = node {
-            1 + Solution::traverse(&n.borrow().left) + Solution::traverse(&n.borrow().right)
+        if let Some(r) = root {
+            let ld = {
+                let mut h = 1;
+                let mut node = r.borrow().left.clone();
+                while let Some(n) = node {
+                    h += 1;
+                    node = n.borrow().left.clone();
+                }
+                h
+            };
+            let rd = {
+                let mut h = 1;
+                let mut node = r.borrow().right.clone();
+                while let Some(n) = node {
+                    h += 1;
+                    node = n.borrow().right.clone();
+                }
+                h
+            };
+            if ld == rd {
+                2i32.pow(ld) - 1
+            } else {
+                1 + Solution::count_nodes(r.borrow().left.clone())
+                    + Solution::count_nodes(r.borrow().right.clone())
+            }
         } else {
             0
         }
