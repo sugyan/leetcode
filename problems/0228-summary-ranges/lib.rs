331081d0b62a8cc2f9a3777b66568e431c4ad239
sugyan <sugi1982@gmail.com>
2020-02-23 16:22:04 +0900

    Add 228
    https://leetcode.com/problems/summary-ranges/

---
+++ problems/0228-summary-ranges/lib.rs
@@ -0,0 +1,43 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
+        let mut answer = Vec::new();
+        let mut i = 0;
+        while i < nums.len() {
+            let n = nums[i];
+            while i < nums.len() - 1 && nums[i + 1] == nums[i] + 1 {
+                i += 1;
+            }
+            let mut s = n.to_string();
+            if n != nums[i] {
+                s += "->";
+                s += &nums[i].to_string();
+            };
+            answer.push(s);
+            i += 1;
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            vec!["0->2", "4->5", "7"],
+            Solution::summary_ranges(vec![0, 1, 2, 4, 5, 7])
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            vec!["0", "2->4", "6", "8->9"],
+            Solution::summary_ranges(vec![0, 2, 3, 4, 6, 8, 9])
+        );
+    }
+}
