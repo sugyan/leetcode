b03065e7248d18774ed0bba6a06eb25b85563e61
sugyan <sugi1982@gmail.com>
2020-03-06 14:09:55 +0900

    Add 275
    https://leetcode.com/problems/h-index-ii/

---
+++ problems/0275-h-index-ii/lib.rs
@@ -0,0 +1,26 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let (mut l, mut r) = (0, citations.len());
+        while l < r {
+            let m = (l + r) / 2;
+            if citations[citations.len() - m - 1] > m as i32 {
+                l = m + 1
+            } else {
+                r = m
+            }
+        }
+        l as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(3, Solution::h_index(vec![0, 1, 3, 5, 6]));
+    }
+}
