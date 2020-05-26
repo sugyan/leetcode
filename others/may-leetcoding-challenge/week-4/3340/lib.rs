c5b2517de75cb396249fa06099015a1944b22d05
sugyan <sugi1982@gmail.com>
2020-05-26 13:48:17 +0900

    Improve may-leetcoding-challenge/week-4/3340

--- others/may-leetcoding-challenge/week-4/3340/lib.rs
+++ others/may-leetcoding-challenge/week-4/3340/lib.rs
@@ -2,17 +2,20 @@ pub struct Solution {}
 
 impl Solution {
     pub fn max_uncrossed_lines(a: Vec<i32>, b: Vec<i32>) -> i32 {
-        let mut dp: Vec<Vec<usize>> = vec![vec![0; b.len() + 1]; a.len() + 1];
-        for i in 0..a.len() {
-            for j in 0..b.len() {
-                dp[i + 1][j + 1] = if a[i] == b[j] {
-                    dp[i][j] + 1
+        let mut dp: Vec<usize> = vec![0; b.len() + 1];
+        for (_, na) in a.iter().enumerate() {
+            let mut prev = 0;
+            for (j, nb) in b.iter().enumerate() {
+                let curr = dp[j + 1];
+                dp[j + 1] = if na == nb {
+                    prev + 1
                 } else {
-                    std::cmp::max(dp[i + 1][j], dp[i][j + 1])
+                    std::cmp::max(dp[j], dp[j + 1])
                 };
+                prev = curr;
             }
         }
-        *dp.last().unwrap().last().unwrap() as i32
+        dp[dp.len() - 1] as i32
     }
 }
 
