4a42cce7224ba08023a9143ca31d7a6c204be841
sugyan <sugi1982@gmail.com>
2020-06-02 17:29:05 +0900

    Add june-leetcoding-challenge/week-1/3348

---
+++ others/june-leetcoding-challenge/week-1/3348/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {
+    }
+};
+
+class Solution {
+   public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
