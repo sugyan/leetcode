1ce146fe0bfcff7cbd235e55939d69c743dccad2
sugyan <sugi1982@gmail.com>
2020-07-05 16:14:36 +0900

    Add july-leetcoding-challenge/week-1/3381

---
+++ others/july-leetcoding-challenge/week-1/3381/lib.rs
@@ -0,0 +1,17 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn hamming_distance(x: i32, y: i32) -> i32 {
+        (x ^ y).count_ones() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(2, Solution::hamming_distance(1, 4));
+    }
+}
