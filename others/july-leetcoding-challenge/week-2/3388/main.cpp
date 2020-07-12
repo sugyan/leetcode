c125c1b0f7958011b8b3e314978e4631a29a49bd
sugyan <sugi1982@gmail.com>
2020-07-12 17:57:42 +0900

    Add july-leetcoding-challenge/week-2/3388

---
+++ others/july-leetcoding-challenge/week-2/3388/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+   public:
+    uint32_t reverseBits(uint32_t n) {
+        n = ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1);
+        n = ((n & 0xCCCCCCCC) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4);
+        n = ((n & 0xFF00FF00) >> 8) | ((n & 0x00FF00FF) << 8);
+        return (n >> 16) | (n << 16);
+    }
+};
