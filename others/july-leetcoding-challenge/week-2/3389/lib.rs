c77233a423e5b35c659c2debf2e830243321b375
sugyan <sugi1982@gmail.com>
2020-07-13 17:05:46 +0900

    Add july-leetcoding-challenge/week-2/3389

---
+++ others/july-leetcoding-challenge/week-2/3389/lib.rs
@@ -0,0 +1,66 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use utils::TreeNode;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        Solution::helper(&p, &q)
+    }
+    fn helper(p: &Option<Rc<RefCell<TreeNode>>>, q: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if p.is_none() && q.is_none() {
+            return true;
+        }
+        if let Some(p) = p {
+            if let Some(q) = q {
+                return p.borrow().val == q.borrow().val
+                    && Solution::helper(&p.borrow().left, &q.borrow().left)
+                    && Solution::helper(&p.borrow().right, &q.borrow().right);
+            }
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utils::to_tree;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            true,
+            Solution::is_same_tree(
+                to_tree(vec![Some(1), Some(2), Some(3)]),
+                to_tree(vec![Some(1), Some(2), Some(3)]),
+            )
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            false,
+            Solution::is_same_tree(
+                to_tree(vec![Some(1), Some(2)]),
+                to_tree(vec![Some(1), None, Some(2)]),
+            )
+        );
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(
+            false,
+            Solution::is_same_tree(
+                to_tree(vec![Some(1), Some(2), Some(1)]),
+                to_tree(vec![Some(1), Some(1), Some(2)]),
+            )
+        );
+    }
+}
