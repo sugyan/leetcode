7746303c34626b48369f1973511be82a68efa7ff
sugyan <sugi1982@gmail.com>
2020-07-28 21:45:57 +0900

    Update july-leetcoding-challenge/week-4/3404

--- others/july-leetcoding-challenge/week-4/3404/lib.rs
+++ others/july-leetcoding-challenge/week-4/3404/lib.rs
@@ -3,18 +3,16 @@ pub struct Solution {}
 impl Solution {
     pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
         let mut d: [i32; 26] = [0; 26];
-        for &task in tasks.iter() {
-            d[(task as u8 - b'A') as usize] += 1;
-        }
         let (mut val, mut num) = (0, 0);
-        for &v in d.iter() {
-            match v.cmp(&val) {
-                std::cmp::Ordering::Less => {}
-                std::cmp::Ordering::Equal => num += 1,
-                std::cmp::Ordering::Greater => {
-                    val = v;
-                    num = 1;
+        for &task in tasks.iter() {
+            let i = (task as u8 - b'A') as usize;
+            d[i] += 1;
+            if d[i] >= val {
+                if d[i] > val {
+                    val = d[i];
+                    num = 0;
                 }
+                num += 1;
             }
         }
         std::cmp::max(tasks.len() as i32, (val - 1) * (n + 1) + num)
