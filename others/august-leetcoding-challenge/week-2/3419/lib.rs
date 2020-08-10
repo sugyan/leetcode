71bd8f6665ddb3472b526d834a963865aaeb0fab
sugyan <sugi1982@gmail.com>
2020-08-10 20:19:04 +0900

    Add august-leetcoding-challenge/week-2/3419

---
+++ others/august-leetcoding-challenge/week-2/3419/lib.rs
@@ -0,0 +1,31 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn title_to_number(s: String) -> i32 {
+        let mut answer = 0;
+        for c in s.as_bytes() {
+            answer = answer * 26 + (c - b'A') as i32 + 1;
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(1, Solution::title_to_number("A".to_string()));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(28, Solution::title_to_number("AB".to_string()));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(701, Solution::title_to_number("ZY".to_string()));
+    }
+}
