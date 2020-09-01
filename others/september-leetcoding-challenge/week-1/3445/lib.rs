a7ba0c46b488941b2051448c9e8a85d88ff98315
sugyan <sugi1982@gmail.com>
2020-09-01 19:56:58 +0900

    Update september-leetcoding-challenge/week-1/3445

--- others/september-leetcoding-challenge/week-1/3445/lib.rs
+++ others/september-leetcoding-challenge/week-1/3445/lib.rs
@@ -3,29 +3,26 @@ pub struct Solution {}
 impl Solution {
     pub fn largest_time_from_digits(a: Vec<i32>) -> String {
         let mut a = a;
-        let mut v: Vec<i32> = Vec::new();
         let mut answers: Vec<(i32, i32)> = Vec::new();
-        Solution::helper(&mut a, &mut v, &mut answers);
+        Solution::helper(&mut a, 0, &mut answers);
         if let Some(max) = answers.iter().max() {
             format!("{:02}:{:02}", max.0, max.1)
         } else {
             String::new()
         }
     }
-    fn helper(a: &mut [i32], v: &mut Vec<i32>, answers: &mut Vec<(i32, i32)>) {
-        if a.is_empty() {
-            let (h, m) = (v[0] * 10 + v[1], v[2] * 10 + v[3]);
+    fn helper(a: &mut [i32], i: usize, answers: &mut Vec<(i32, i32)>) {
+        if i == 4 {
+            let (h, m) = (a[0] * 10 + a[1], a[2] * 10 + a[3]);
             if h < 24 && m < 60 {
                 answers.push((h, m));
             }
             return;
         }
-        for i in 0..a.len() {
-            v.push(a[i]);
-            a.swap(0, i);
-            Solution::helper(&mut a[1..], v, answers);
-            a.swap(0, i);
-            v.pop();
+        for j in i..4 {
+            a.swap(i, j);
+            Solution::helper(a, i + 1, answers);
+            a.swap(i, j);
         }
     }
 }
