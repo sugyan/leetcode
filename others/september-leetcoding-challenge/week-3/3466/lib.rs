f889d3a9c6f31cde4231ff1263e27d8d6024b8a4
sugyan <sugi1982@gmail.com>
2020-09-20 21:56:31 +0900

    Update september-leetcoding-challenge/week-3/3466

--- others/september-leetcoding-challenge/week-3/3466/lib.rs
+++ others/september-leetcoding-challenge/week-3/3466/lib.rs
@@ -3,42 +3,41 @@ pub struct Solution {}
 impl Solution {
     pub fn unique_paths_iii(grid: Vec<Vec<i32>>) -> i32 {
         let mut grid = grid;
-        let count: usize = grid
-            .iter()
-            .map(|row| row.iter().filter(|&col| *col == 0).count())
-            .sum();
+        let mut pos = (0, 0);
+        let mut count = 0;
         for i in 0..grid.len() {
             for j in 0..grid[0].len() {
-                if grid[i][j] == 1 {
-                    return Solution::helper(&mut grid, (i, j), count);
+                match grid[i][j] {
+                    0 => count += 1,
+                    1 => pos = (i, j),
+                    _ => {}
                 }
             }
         }
-        0
+        Solution::helper(&mut grid, pos, count + 1)
     }
     fn helper(grid: &mut Vec<Vec<i32>>, pos: (usize, usize), count: usize) -> i32 {
-        if count == 0
-            && ((pos.0 > 0 && grid[pos.0 - 1][pos.1] == 2)
-                || (pos.1 > 0 && grid[pos.0][pos.1 - 1] == 2)
-                || (pos.0 < grid.len() - 1 && grid[pos.0 + 1][pos.1] == 2)
-                || (pos.1 < grid[0].len() - 1 && grid[pos.0][pos.1 + 1] == 2))
-        {
-            return 1;
+        if grid[pos.0][pos.1] == 2 {
+            return if count == 0 { 1 } else { 0 };
         }
-        let mut ret = 0;
         grid[pos.0][pos.1] = 1;
-        if pos.0 > 0 && grid[pos.0 - 1][pos.1] == 0 {
-            ret += Solution::helper(grid, (pos.0 - 1, pos.1), count - 1);
-        }
-        if pos.1 > 0 && grid[pos.0][pos.1 - 1] == 0 {
-            ret += Solution::helper(grid, (pos.0, pos.1 - 1), count - 1);
-        }
-        if pos.0 < grid.len() - 1 && grid[pos.0 + 1][pos.1] == 0 {
-            ret += Solution::helper(grid, (pos.0 + 1, pos.1), count - 1);
-        }
-        if pos.1 < grid[0].len() - 1 && grid[pos.0][pos.1 + 1] == 0 {
-            ret += Solution::helper(grid, (pos.0, pos.1 + 1), count - 1);
-        }
+        let ret = if pos.0 > 0 && grid[pos.0 - 1][pos.1].abs() != 1 {
+            Solution::helper(grid, (pos.0 - 1, pos.1), count - 1)
+        } else {
+            0
+        } + if pos.1 > 0 && grid[pos.0][pos.1 - 1].abs() != 1 {
+            Solution::helper(grid, (pos.0, pos.1 - 1), count - 1)
+        } else {
+            0
+        } + if pos.0 < grid.len() - 1 && grid[pos.0 + 1][pos.1].abs() != 1 {
+            Solution::helper(grid, (pos.0 + 1, pos.1), count - 1)
+        } else {
+            0
+        } + if pos.1 < grid[0].len() - 1 && grid[pos.0][pos.1 + 1].abs() != 1 {
+            Solution::helper(grid, (pos.0, pos.1 + 1), count - 1)
+        } else {
+            0
+        };
         grid[pos.0][pos.1] = 0;
         ret
     }
