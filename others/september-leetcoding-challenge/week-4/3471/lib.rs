8a69d66ce38ee0e70d933be586e26bfa40987b3a
sugyan <sugi1982@gmail.com>
2020-09-24 17:11:30 +0900

    Update september-leetcoding-challenge/week-4/3471

--- others/september-leetcoding-challenge/week-4/3471/lib.rs
+++ others/september-leetcoding-challenge/week-4/3471/lib.rs
@@ -2,14 +2,14 @@ pub struct Solution {}
 
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut d = [0; 26];
-        for c in t.as_bytes() {
-            d[(c - b'a') as usize] += 1;
-        }
+        let mut answer = 0;
         for c in s.as_bytes() {
-            d[(c - b'a') as usize] -= 1;
+            answer ^= c;
         }
-        (b'a' + (0..26).find(|&i| d[i] == 1).unwrap() as u8) as char
+        for c in t.as_bytes() {
+            answer ^= c;
+        }
+        answer as char
     }
 }
 
