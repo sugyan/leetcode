9a3c20b2f9c7864f7f32560bcfbe607b7c710deb
sugyan <sugi1982@gmail.com>
2020-10-02 18:06:50 +0900

    Add october-leetcoding-challenge/week-1/3481

---
+++ others/october-leetcoding-challenge/week-1/3481/lib.rs
@@ -0,0 +1,85 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut answer = Vec::new();
+        let mut v: Vec<i32> = Vec::new();
+        Solution::helper(&candidates, target, 0, &mut v, &mut answer);
+        answer
+    }
+    fn helper(
+        candidates: &[i32],
+        target: i32,
+        i: usize,
+        v: &mut Vec<i32>,
+        answer: &mut Vec<Vec<i32>>,
+    ) {
+        if target == 0 {
+            answer.push(v.clone());
+            return;
+        }
+        for j in i..candidates.len() {
+            if candidates[j] <= target {
+                v.push(candidates[j]);
+                Solution::helper(candidates, target - candidates[j], j, v, answer);
+                v.pop();
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut ret = Solution::combination_sum(vec![2, 3, 6, 7], 7);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        assert_eq!(vec![vec![2, 2, 3], vec![7]], ret);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut ret = Solution::combination_sum(vec![2, 3, 5], 8);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        assert_eq!(vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]], ret);
+    }
+
+    #[test]
+    fn example_3() {
+        let mut ret = Solution::combination_sum(vec![2], 1);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        let v: Vec<Vec<i32>> = Vec::new();
+        assert_eq!(v, ret);
+    }
+
+    #[test]
+    fn example_4() {
+        let mut ret = Solution::combination_sum(vec![1], 1);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        assert_eq!(vec![vec![1]], ret);
+    }
+
+    #[test]
+    fn example_5() {
+        let mut ret = Solution::combination_sum(vec![1], 2);
+        for v in ret.iter_mut() {
+            v.sort();
+        }
+        ret.sort();
+        assert_eq!(vec![vec![1, 1]], ret);
+    }
+}
