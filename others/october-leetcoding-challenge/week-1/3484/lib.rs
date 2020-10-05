7b8a06443e696475e9f51a52b7757bdf53f5eba9
sugyan <sugi1982@gmail.com>
2020-10-05 17:03:57 +0900

    Update october-leetcoding-challenge/week-1/3484

--- others/october-leetcoding-challenge/week-1/3484/lib.rs
+++ others/october-leetcoding-challenge/week-1/3484/lib.rs
@@ -2,14 +2,11 @@ pub struct Solution {}
 
 impl Solution {
     pub fn bitwise_complement(n: i32) -> i32 {
-        if n == 0 {
-            return 1;
-        }
         let mut m = 1;
-        while m <= n {
-            m <<= 1;
+        while m < n {
+            m = (m << 1) + 1
         }
-        m - 1 - n
+        m - n
     }
 }
 
