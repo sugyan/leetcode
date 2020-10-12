f3dceb0a221bcfe4d726b5aa5d5d3b5b65aff25d
sugyan <sugi1982@gmail.com>
2020-10-12 22:23:58 +0900

    Update october-leetcoding-challenge/week-2/3492

--- others/october-leetcoding-challenge/week-2/3492/lib.rs
+++ others/october-leetcoding-challenge/week-2/3492/lib.rs
@@ -5,21 +5,23 @@ impl Solution {
         if a.len() != b.len() {
             return false;
         }
-        let mut counts = [0; 26];
-        let mut diff = Vec::new();
-        let mut a: Vec<char> = a.chars().collect();
-        for (i, (&ba, bb)) in a.iter().zip(b.chars()).enumerate() {
-            counts[(ba as u8 - b'a') as usize] += 1;
-            if ba != bb {
-                diff.push(i);
-            }
-        }
+        let diff: Vec<(char, char)> = a
+            .chars()
+            .zip(b.chars())
+            .filter(|(ca, cb)| ca != cb)
+            .collect();
         match diff.len() {
-            0 => counts.iter().any(|&n| n > 1),
-            2 => {
-                a.swap(diff[0], diff[1]);
-                a.iter().collect::<String>() == b
+            0 => {
+                let mut counts = [0; 26];
+                for b in a.as_bytes().iter() {
+                    counts[(b - b'a') as usize] += 1;
+                    if counts[(b - b'a') as usize] > 1 {
+                        return true;
+                    }
+                }
+                false
             }
+            2 => diff[0].0 == diff[1].1 && diff[0].1 == diff[1].0,
             _ => false,
         }
     }
