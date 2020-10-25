03f1112a644ddb3adf7ed38ad63c5ff90bd8d3d9
sugyan <sugi1982@gmail.com>
2020-10-25 21:47:32 +0900

    Add october-leetcoding-challenge/week-4/3507

---
+++ others/october-leetcoding-challenge/week-4/3507/lib.rs
@@ -0,0 +1,48 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn winner_square_game(n: i32) -> bool {
+        let mut v = vec![false; n as usize + 1];
+        for i in 0..=n as usize {
+            if i == 0 || (v[i - 1] && !v[i]) {
+                for j in 1.. {
+                    if i + j * j > n as usize {
+                        break;
+                    }
+                    v[i + j * j] = true;
+                }
+            }
+        }
+        v[n as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(true, Solution::winner_square_game(1));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(false, Solution::winner_square_game(2));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(true, Solution::winner_square_game(4));
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(false, Solution::winner_square_game(7));
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(false, Solution::winner_square_game(17));
+    }
+}
