43572f40b3b973fc06fcad6fee0ec1575144de6e
sugyan <sugi1982@gmail.com>
2020-11-11 22:44:46 +0900

    Update november-leetcoding-challenge/week-2/3527

--- others/november-leetcoding-challenge/week-2/3527/lib.rs
+++ others/november-leetcoding-challenge/week-2/3527/lib.rs
@@ -1,35 +1,27 @@
+use std::collections::HashMap;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn valid_square(p1: Vec<i32>, p2: Vec<i32>, p3: Vec<i32>, p4: Vec<i32>) -> bool {
-        let v12 = vec![p2[0] - p1[0], p2[1] - p1[1]];
-        let v13 = vec![p3[0] - p1[0], p3[1] - p1[1]];
-        let v14 = vec![p4[0] - p1[0], p4[1] - p1[1]];
-        if (v12[0] == 0 && v12[1] == 0)
-            || (v13[0] == 0 && v13[1] == 0)
-            || (v14[0] == 0 && v14[1] == 0)
-        {
-            return false;
-        }
-        if v12[0] * v13[0] + v12[1] * v13[1] == 0
-            && v12[0] * v12[0] + v12[1] * v12[1] == v13[0] * v13[0] + v13[1] * v13[1]
-            && (p1[0] + v12[0] + v13[0] == p4[0] && p1[1] + v12[1] + v13[1] == p4[1])
-        {
-            return true;
-        }
-        if v13[0] * v14[0] + v13[1] * v14[1] == 0
-            && v13[0] * v13[0] + v13[1] * v13[1] == v14[0] * v14[0] + v14[1] * v14[1]
-            && (p1[0] + v13[0] + v14[0] == p2[0] && p1[1] + v13[1] + v14[1] == p2[1])
-        {
-            return true;
-        }
-        if v14[0] * v12[0] + v14[1] * v12[1] == 0
-            && v14[0] * v14[0] + v14[1] * v14[1] == v12[0] * v12[0] + v12[1] * v12[1]
-            && (p1[0] + v14[0] + v12[0] == p3[0] && p1[1] + v14[1] + v12[1] == p3[1])
-        {
-            return true;
-        }
-        false
+        let mut hm: HashMap<i32, usize> = HashMap::new();
+        let dist = |p: &Vec<i32>, q: &Vec<i32>| -> i32 {
+            let x = q[0] - p[0];
+            let y = q[1] - p[1];
+            x * x + y * y
+        };
+        [
+            dist(&p1, &p2),
+            dist(&p1, &p3),
+            dist(&p1, &p4),
+            dist(&p2, &p3),
+            dist(&p2, &p4),
+            dist(&p3, &p4),
+        ]
+        .iter()
+        .for_each(|&d| *hm.entry(d).or_insert(0) += 1);
+        let values: Vec<usize> = hm.iter().map(|(_, &v)| v).collect();
+        values == vec![2, 4] || values == vec![4, 2]
     }
 }
 
