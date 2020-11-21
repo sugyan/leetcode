b1fb1e937a1e304d44ffb645257184476133ee41
sugyan <sugi1982@gmail.com>
2020-11-21 23:39:40 +0900

    Update november-leetcoding-challenge/week-3/3538

--- others/november-leetcoding-challenge/week-3/3538/lib.rs
+++ others/november-leetcoding-challenge/week-3/3538/lib.rs
@@ -2,24 +2,23 @@ pub struct Solution {}
 
 impl Solution {
     pub fn at_most_n_given_digit_set(digits: Vec<String>, n: i32) -> i32 {
+        let digits: Vec<char> = digits.iter().map(|s| s.chars().next().unwrap()).collect();
         let n = n.to_string();
-        let mut digits: Vec<char> = digits.iter().map(|s| s.chars().next().unwrap()).collect();
-        digits.sort_unstable();
-        let mut answer = 0;
-        for i in 0..n.len() - 1 {
-            answer += digits.len().pow(i as u32 + 1) as i32;
-        }
-        for (i, c) in n.chars().enumerate() {
-            match digits.binary_search(&c) {
-                Ok(j) => {
-                    answer += (j * digits.len().pow((n.len() - i - 1) as u32)) as i32;
-                }
-                Err(j) => {
-                    return answer + (j * digits.len().pow((n.len() - i - 1) as u32)) as i32;
+        let mut dp = vec![0; n.len() + 1];
+        dp[n.len()] = 1;
+        for (i, c) in n.chars().rev().enumerate() {
+            for &d in digits.iter() {
+                match d.cmp(&c) {
+                    std::cmp::Ordering::Less => dp[n.len() - i - 1] += digits.len().pow(i as u32),
+                    std::cmp::Ordering::Equal => dp[n.len() - i - 1] += dp[n.len() - i],
+                    std::cmp::Ordering::Greater => {}
                 }
             }
         }
-        answer + 1
+        (dp[0]
+            + (1..n.len())
+                .map(|i| digits.len().pow(i as u32))
+                .sum::<usize>()) as i32
     }
 }
 
