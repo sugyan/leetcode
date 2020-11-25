d34022ccd5112635646c2d5efd5723493c450634
sugyan <sugi1982@gmail.com>
2020-11-25 20:56:03 +0900

    Update november-leetcoding-challenge/week-4/3543

--- others/november-leetcoding-challenge/week-4/3543/lib.rs
+++ others/november-leetcoding-challenge/week-4/3543/lib.rs
@@ -2,13 +2,12 @@ pub struct Solution {}
 
 impl Solution {
     pub fn smallest_repunit_div_by_k(k: i32) -> i32 {
-        let mut m = 1;
+        let mut m = 0;
         for i in 1..=k {
-            m %= k;
+            m = (m * 10 + 1) % k;
             if m == 0 {
                 return i;
             }
-            m = m * 10 + 1;
         }
         -1
     }
