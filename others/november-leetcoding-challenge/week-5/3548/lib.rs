a0ce5102178acd52f19b3b0733aeb82a61b251a5
sugyan <sugi1982@gmail.com>
2020-11-29 22:48:07 +0900

    Update november-leetcoding-challenge/week-5/3548 (4ms -> 0ms)

--- others/november-leetcoding-challenge/week-5/3548/lib.rs
+++ others/november-leetcoding-challenge/week-5/3548/lib.rs
@@ -1,20 +1,24 @@
-use std::collections::HashSet;
+use std::collections::VecDeque;
 
 pub struct Solution {}
 
 impl Solution {
     pub fn can_reach(arr: Vec<i32>, start: i32) -> bool {
-        let mut hs: HashSet<i32> = HashSet::new();
-        let mut stack: Vec<i32> = Vec::new();
-        stack.push(start);
-        while let Some(last) = stack.pop() {
-            if arr[last as usize] == 0 {
-                return true;
-            }
-            for &i in [last + arr[last as usize], last - arr[last as usize]].iter() {
-                if i >= 0 && (i as usize) < arr.len() && !hs.contains(&i) {
-                    hs.insert(i);
-                    stack.push(i);
+        let mut arr = arr;
+        let mut vd: VecDeque<i32> = VecDeque::with_capacity(arr.len());
+        vd.push_back(start);
+        while let Some(front) = vd.pop_front() {
+            match arr[front as usize].cmp(&0) {
+                std::cmp::Ordering::Less => continue,
+                std::cmp::Ordering::Equal => return true,
+                std::cmp::Ordering::Greater => {
+                    if front - arr[front as usize] >= 0 {
+                        vd.push_back(front - arr[front as usize]);
+                    }
+                    if front + arr[front as usize] < arr.len() as i32 {
+                        vd.push_back(front + arr[front as usize]);
+                    }
+                    arr[front as usize] = -1;
                 }
             }
         }
