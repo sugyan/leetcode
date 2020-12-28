fe0ef9cca98797a485c5a3752e67708e2cc57291
sugyan <sugi1982@gmail.com>
2020-12-28 17:50:38 +0900

    Update december-leetcoding-challenge/week-4/3583

--- others/december-leetcoding-challenge/week-4/3583/lib.rs
+++ others/december-leetcoding-challenge/week-4/3583/lib.rs
@@ -2,8 +2,8 @@ pub struct Solution {}
 
 impl Solution {
     pub fn reach_number(target: i32) -> i32 {
-        let target = if target < 0 { -target } else { target };
-        for n in std::cmp::max((target as f32 * 2.0).sqrt() as i32, 1) - 1.. {
+        let target = target.abs();
+        for n in (-1.0 + (1.0 + 8.0 * target as f32).sqrt()) as i32 / 2.. {
             let m = n * (n + 1) / 2;
             if m >= target && (m - target) % 2 == 0 {
                 return n;
