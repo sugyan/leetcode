5a6a34ca8fd7711d00c0635a4d401a8298217238
sugyan <sugi1982@gmail.com>
2020-12-30 17:59:52 +0900

    Update december-leetcoding-challenge/week-5/3586

--- others/december-leetcoding-challenge/week-5/3586/lib.rs
+++ others/december-leetcoding-challenge/week-5/3586/lib.rs
@@ -2,40 +2,34 @@ pub struct Solution {}
 
 impl Solution {
     pub fn game_of_life(board: &mut Vec<Vec<i32>>) {
-        *board = board
-            .iter()
-            .enumerate()
-            .map(|(i, row)| {
-                row.iter()
-                    .enumerate()
-                    .map(|(j, &col)| {
-                        let count = [
-                            (-1, -1),
-                            (-1, 0),
-                            (-1, 1),
-                            (0, -1),
-                            (0, 1),
-                            (1, -1),
-                            (1, 0),
-                            (1, 1),
-                        ]
-                        .iter()
-                        .filter(|&d| {
-                            let (ii, jj) = (i as i32 + d.0, j as i32 + d.1);
-                            (0..board.len() as i32).contains(&ii)
-                                && (0..board[0].len() as i32).contains(&jj)
-                                && board[ii as usize][jj as usize] == 1
-                        })
-                        .count();
-                        if count == 3 || (col == 1 && count == 2) {
-                            1
-                        } else {
-                            0
-                        }
-                    })
-                    .collect()
-            })
-            .collect();
+        for i in 0..board.len() {
+            for j in 0..board[0].len() {
+                let count = [
+                    (-1, -1),
+                    (-1, 0),
+                    (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                ]
+                .iter()
+                .filter(|&d| {
+                    let (ii, jj) = (i as i32 + d.0, j as i32 + d.1);
+                    (0..board.len() as i32).contains(&ii)
+                        && (0..board[0].len() as i32).contains(&jj)
+                        && board[ii as usize][jj as usize] & 1 != 0
+                })
+                .count();
+                if count == 3 || (board[i][j] & 1 != 0 && count == 2) {
+                    board[i][j] |= 1 << 1;
+                }
+            }
+        }
+        board
+            .iter_mut()
+            .for_each(|row| row.iter_mut().for_each(|col| *col >>= 1));
     }
 }
 
