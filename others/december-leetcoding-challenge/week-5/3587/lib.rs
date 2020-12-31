14fa9fa6ed75ac49f252f4bd171223ccb0bec9c4
sugyan <sugi1982@gmail.com>
2020-12-31 23:27:16 +0900

    Update others/december-leetcoding-challenge/week-5/3587 (1080ms -> 0ms)

--- others/december-leetcoding-challenge/week-5/3587/lib.rs
+++ others/december-leetcoding-challenge/week-5/3587/lib.rs
@@ -1,29 +1,30 @@
-use std::collections::HashMap;
-
 pub struct Solution {}
 
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        let mut answer = 0;
-        let mut hm = HashMap::new();
-        for (i, &height) in heights.iter().enumerate() {
-            hm.insert(
-                height,
-                if let Some(min) = hm
-                    .iter()
-                    .filter(|(&k, _)| k >= height)
-                    .map(|(_, &v)| v)
-                    .min()
-                {
-                    min
-                } else {
-                    i
-                },
-            );
-            hm.retain(|&k, _| k <= height);
-            for (&k, &v) in hm.iter() {
-                answer = std::cmp::max(answer, (i + 1 - v) as i32 * k);
+        if heights.is_empty() {
+            return 0;
+        }
+        let mut lr = vec![(0, 0); heights.len()];
+        lr[0].0 = -1;
+        for i in 1..heights.len() {
+            let mut p = i as i32 - 1;
+            while p >= 0 && heights[p as usize] >= heights[i] {
+                p = lr[p as usize].0;
             }
+            lr[i].0 = p;
+        }
+        lr[heights.len() - 1].1 = heights.len() as i32;
+        for i in (0..heights.len() - 1).rev() {
+            let mut p = i as i32 + 1;
+            while (p as usize) < heights.len() && heights[p as usize] >= heights[i] {
+                p = lr[p as usize].1;
+            }
+            lr[i].1 = p;
+        }
+        let mut answer = 0;
+        for (i, &(l, r)) in lr.iter().enumerate() {
+            answer = std::cmp::max(answer, heights[i] * (r - l - 1));
         }
         answer
     }
