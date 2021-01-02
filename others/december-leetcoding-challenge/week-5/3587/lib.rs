e508a69399d5a7c9eca72953a7e67c3a8ea06024
sugyan <sugi1982@gmail.com>
2021-01-02 21:11:50 +0900

    Updaate others/december-leetcoding-challenge/week-5/3587

--- others/december-leetcoding-challenge/week-5/3587/lib.rs
+++ others/december-leetcoding-challenge/week-5/3587/lib.rs
@@ -2,29 +2,29 @@ pub struct Solution {}
 
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        if heights.is_empty() {
-            return 0;
-        }
-        let mut lr = vec![(0, 0); heights.len()];
-        lr[0].0 = -1;
-        for i in 1..heights.len() {
-            let mut p = i as i32 - 1;
-            while p >= 0 && heights[p as usize] >= heights[i] {
-                p = lr[p as usize].0;
-            }
-            lr[i].0 = p;
-        }
-        lr[heights.len() - 1].1 = heights.len() as i32;
-        for i in (0..heights.len() - 1).rev() {
-            let mut p = i as i32 + 1;
-            while (p as usize) < heights.len() && heights[p as usize] >= heights[i] {
-                p = lr[p as usize].1;
-            }
-            lr[i].1 = p;
-        }
+        let mut heights = heights;
+        heights.push(0);
+        let mut stack = Vec::new();
         let mut answer = 0;
-        for (i, &(l, r)) in lr.iter().enumerate() {
-            answer = std::cmp::max(answer, heights[i] * (r - l - 1));
+        for i in 0..heights.len() {
+            while !stack.is_empty() {
+                if let Some(&last) = stack.last() {
+                    if heights[i] >= heights[last] {
+                        break;
+                    }
+                    if let Some(p) = stack.pop() {
+                        let w = i as i32
+                            - 1
+                            - if let Some(&l) = stack.last() {
+                                l as i32
+                            } else {
+                                -1
+                            };
+                        answer = std::cmp::max(answer, heights[p] * w);
+                    }
+                }
+            }
+            stack.push(i);
         }
         answer
     }
