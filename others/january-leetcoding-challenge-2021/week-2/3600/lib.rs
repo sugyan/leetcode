96f01e15bde111ff669f494c1db52e5018475c42
sugyan <sugi1982@gmail.com>
2021-01-11 18:09:08 +0900

    Add january-leetcoding-challenge-2021/week-2/3600

---
+++ others/january-leetcoding-challenge-2021/week-2/3600/lib.rs
@@ -0,0 +1,46 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut m = m as usize;
+        let mut n = n as usize;
+        while m > 0 && n > 0 {
+            if nums1[m - 1] > nums2[n - 1] {
+                nums1[m + n - 1] = nums1[m - 1];
+                m -= 1;
+            } else {
+                nums1[m + n - 1] = nums2[n - 1];
+                n -= 1;
+            }
+        }
+        while n > 0 {
+            nums1[n - 1] = nums2[n - 1];
+            n -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+        let m = 3;
+        let mut nums2 = vec![2, 5, 6];
+        let n = 3;
+        Solution::merge(&mut nums1, m, &mut nums2, n);
+        assert_eq!(vec![1, 2, 2, 3, 5, 6], nums1);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut nums1 = vec![1];
+        let m = 1;
+        let mut nums2 = vec![];
+        let n = 0;
+        Solution::merge(&mut nums1, m, &mut nums2, n);
+        assert_eq!(vec![1], nums1);
+    }
+}
