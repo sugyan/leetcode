6759954bb6262b6ee4b6a45ec0bf254610e261d6
sugyan <sugi1982@gmail.com>
2021-01-11 18:14:24 +0900

    Update january-leetcoding-challenge-2021/week-2/3600

--- others/january-leetcoding-challenge-2021/week-2/3600/lib.rs
+++ others/january-leetcoding-challenge-2021/week-2/3600/lib.rs
@@ -4,18 +4,14 @@ impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         let mut m = m as usize;
         let mut n = n as usize;
-        while m > 0 && n > 0 {
-            if nums1[m - 1] > nums2[n - 1] {
+        while n > 0 {
+            if m > 0 && nums1[m - 1] > nums2[n - 1] {
                 nums1[m + n - 1] = nums1[m - 1];
                 m -= 1;
             } else {
                 nums1[m + n - 1] = nums2[n - 1];
                 n -= 1;
             }
-        }
-        while n > 0 {
-            nums1[n - 1] = nums2[n - 1];
-            n -= 1;
         }
     }
 }
