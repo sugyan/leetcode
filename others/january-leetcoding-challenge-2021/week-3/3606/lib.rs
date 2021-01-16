fee028f59cc61bc95336b2201a073085f18b239d
sugyan <sugi1982@gmail.com>
2021-01-16 17:19:27 +0900

    Update january-leetcoding-challenge-2021/week-3/3606

--- others/january-leetcoding-challenge-2021/week-3/3606/lib.rs
+++ others/january-leetcoding-challenge-2021/week-3/3606/lib.rs
@@ -3,8 +3,25 @@ pub struct Solution;
 impl Solution {
     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
         let mut nums = nums;
-        nums.sort_unstable();
-        nums[nums.len() - k as usize]
+        let lr = (0, nums.len() - 1);
+        Self::quick_select(&mut nums, lr, k as usize)
+    }
+    fn quick_select(nums: &mut [i32], lr: (usize, usize), k: usize) -> i32 {
+        let pivot = (lr.0 + lr.1) / 2;
+        nums.swap(pivot, lr.1);
+        let mut n = lr.0;
+        for i in lr.0..lr.1 {
+            if nums[i] < nums[lr.1] {
+                nums.swap(i, n);
+                n += 1;
+            }
+        }
+        let count = lr.1 - n + 1;
+        match count.cmp(&k) {
+            std::cmp::Ordering::Less => Self::quick_select(nums, (lr.0, n - 1), k - count),
+            std::cmp::Ordering::Equal => nums[lr.1],
+            std::cmp::Ordering::Greater => Self::quick_select(nums, (n, lr.1 - 1), k),
+        }
     }
 }
 
