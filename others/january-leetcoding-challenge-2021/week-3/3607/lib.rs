abbaf2da1fd72e4ecf2ff0883ad16792fc61d566
sugyan <sugi1982@gmail.com>
2021-01-17 17:50:24 +0900

    Update january-leetcoding-challenge-2021/week-3/3607

--- others/january-leetcoding-challenge-2021/week-3/3607/lib.rs
+++ others/january-leetcoding-challenge-2021/week-3/3607/lib.rs
@@ -4,8 +4,8 @@ impl Solution {
     pub fn count_vowel_strings(n: i32) -> i32 {
         let mut v = [1; 5];
         for _ in 1..n {
-            for i in (0..4).rev() {
-                v[i] += v[i + 1];
+            for i in 1..5 {
+                v[i] += v[i - 1];
             }
         }
         v.iter().sum()
