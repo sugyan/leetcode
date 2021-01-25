fa63d82757a52f15d8b7c4268b8aabd60bd4bc12
sugyan <sugi1982@gmail.com>
2021-01-25 17:28:08 +0900

    Add january-leetcoding-challenge-2021/week-4/3616

---
+++ others/january-leetcoding-challenge-2021/week-4/3616/lib.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn k_length_apart(nums: Vec<i32>, k: i32) -> bool {
+        let mut prev = None;
+        for i in (0..nums.len()).filter(|&i| nums[i] == 1) {
+            if let Some(p) = prev {
+                if i - p <= k as usize {
+                    return false;
+                }
+            }
+            prev = Some(i);
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            true,
+            Solution::k_length_apart(vec![1, 0, 0, 0, 1, 0, 0, 1], 2)
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(false, Solution::k_length_apart(vec![1, 0, 0, 1, 0, 1], 2));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(true, Solution::k_length_apart(vec![1, 1, 1, 1, 1], 0));
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(true, Solution::k_length_apart(vec![0, 1, 0, 1], 1));
+    }
+}
