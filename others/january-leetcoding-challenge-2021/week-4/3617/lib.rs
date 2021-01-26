f5d5e799f78cdf72dd2ec63414a65e7a949781dc
sugyan <sugi1982@gmail.com>
2021-01-26 22:18:00 +0900

    Update january-leetcoding-challenge-2021/week-4/3617 (384ms -> 44ms)

--- others/january-leetcoding-challenge-2021/week-4/3617/lib.rs
+++ others/january-leetcoding-challenge-2021/week-4/3617/lib.rs
@@ -1,75 +1,42 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 pub struct Solution;
 
 impl Solution {
     pub fn minimum_effort_path(heights: Vec<Vec<i32>>) -> i32 {
-        let mut graph = HashMap::new();
         let (r, c) = (heights.len(), heights[0].len());
-        for i in 0..r {
-            for j in 0..c {
-                let height = heights[i][j];
-                if i > 0 {
-                    graph
-                        .entry((i, j))
-                        .or_insert_with(Vec::new)
-                        .push(((i - 1, j), (heights[i - 1][j] - height).abs()));
-                }
-                if j > 0 {
-                    graph
-                        .entry((i, j))
-                        .or_insert_with(Vec::new)
-                        .push(((i, j - 1), (heights[i][j - 1] - height).abs()));
-                }
-                if i < r - 1 {
-                    graph
-                        .entry((i, j))
-                        .or_insert_with(Vec::new)
-                        .push(((i + 1, j), (heights[i + 1][j] - height).abs()));
-                }
-                if j < c - 1 {
-                    graph
-                        .entry((i, j))
-                        .or_insert_with(Vec::new)
-                        .push(((i, j + 1), (heights[i][j + 1] - height).abs()));
-                }
+        let mut min_effort = vec![vec![None; c]; r];
+        let mut bh = BinaryHeap::new();
+        bh.push((Reverse(0), (0, 0)));
+        while let Some((Reverse(min), (i, j))) = bh.pop() {
+            if i == r - 1 && j == c - 1 {
+                return min;
             }
-        }
-        let (mut lo, mut hi) = (0, 1_000_000);
-        while lo < hi {
-            let mid = lo + (hi - lo) / 2;
-            if Self::reachable(&graph, (r - 1, c - 1), mid) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        lo
-    }
-    fn reachable(
-        graph: &HashMap<(usize, usize), Vec<((usize, usize), i32)>>,
-        target: (usize, usize),
-        k: i32,
-    ) -> bool {
-        let mut visited = HashSet::new();
-        let mut stack = vec![(0, 0)];
-        while let Some(pos) = stack.pop() {
-            if pos == target {
-                return true;
-            }
-            if visited.contains(&pos) {
+            if min_effort[i][j].is_some() {
                 continue;
+            } else {
+                min_effort[i][j] = Some(min);
             }
-            visited.insert(pos);
-            if let Some(v) = graph.get(&pos) {
-                for (p, effort) in v {
-                    if !visited.contains(p) && *effort <= k {
-                        stack.push(*p);
+            let height = heights[i][j];
+            for (i, j) in [(-1, 0), (0, -1), (1, 0), (0, 1)]
+                .iter()
+                .filter_map(|(di, dj)| {
+                    let (ii, jj) = (i as i32 + di, j as i32 + dj);
+                    if (0..r as i32).contains(&ii) && (0..c as i32).contains(&jj) {
+                        Some((ii as usize, jj as usize))
+                    } else {
+                        None
                     }
-                }
+                })
+            {
+                bh.push((
+                    Reverse(std::cmp::max(min, (heights[i][j] - height).abs())),
+                    (i, j),
+                ));
             }
         }
-        false
+        unreachable!();
     }
 }
 
