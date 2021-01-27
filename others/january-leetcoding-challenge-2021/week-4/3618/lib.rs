dcf187d82138ab5aac233576aac2534488a72557
sugyan <sugi1982@gmail.com>
2021-01-27 17:42:31 +0900

    Update january-leetcoding-challenge-2021/week-4/3618 (292ms -> 16ms)

--- others/january-leetcoding-challenge-2021/week-4/3618/lib.rs
+++ others/january-leetcoding-challenge-2021/week-4/3618/lib.rs
@@ -1,16 +1,16 @@
 pub struct Solution;
 
-const DIV: i32 = 1_000_000_007;
+const DIV: u64 = 1_000_000_007;
 
 impl Solution {
     pub fn concatenated_binary(n: i32) -> i32 {
-        let mut answer = 0;
-        for i in 1..=n {
-            for j in (0..32 - i.leading_zeros()).rev() {
-                answer = ((answer << 1) + if i & 1 << j != 0 { 1 } else { 0 }) % DIV;
-            }
+        let mut answer = 0_u64;
+        for i in 1..=n as u64 {
+            answer <<= 64 - i.leading_zeros();
+            answer += i;
+            answer %= DIV;
         }
-        answer
+        answer as i32
     }
 }
 
