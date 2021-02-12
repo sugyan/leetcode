65dc422edb6182e76a101a9295dd8552acb2ddb0
sugyan <sugi1982@gmail.com>
2021-02-12 17:59:00 +0900

    Add february-leetcoding-challenge-2021/week-2/3637

---
+++ others/february-leetcoding-challenge-2021/week-2/3637/lib.rs
@@ -0,0 +1,27 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn number_of_steps(num: i32) -> i32 {
+        (31 + std::cmp::max(1, num.count_ones()) - num.leading_zeros()) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(6, Solution::number_of_steps(14));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(4, Solution::number_of_steps(8));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(12, Solution::number_of_steps(123));
+    }
+}
