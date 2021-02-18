14ab85c3c1a1b83369f2b7acd7ea0d5e1cc9cce0
sugyan <sugi1982@gmail.com>
2021-02-18 17:21:20 +0900

    Update february-leetcoding-challenge-2021/week-3/3644

--- others/february-leetcoding-challenge-2021/week-3/3644/lib.rs
+++ others/february-leetcoding-challenge-2021/week-3/3644/lib.rs
@@ -2,23 +2,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn number_of_arithmetic_slices(a: Vec<i32>) -> i32 {
-        if a.len() < 3 {
-            return 0;
-        }
         let mut answer = 0;
-        let mut i = 0;
-        while i < a.len() - 2 {
-            if a[i] - a[i + 1] == a[i + 1] - a[i + 2] {
-                let mut n = 1;
-                while i + 3 < a.len() && a[i + 1] - a[i + 2] == a[i + 2] - a[i + 3] {
-                    n += 1;
-                    i += 1;
-                }
+        let mut n = 0;
+        for i in 2..a.len() {
+            if a[i] - a[i - 1] == a[i - 1] - a[i - 2] {
+                n += 1;
+            } else {
                 answer += n * (n + 1) / 2;
+                n = 0;
             }
-            i += 1;
         }
-        answer
+        answer + n * (n + 1) / 2
     }
 }
 
