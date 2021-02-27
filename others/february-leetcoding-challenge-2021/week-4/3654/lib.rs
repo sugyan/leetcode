0341f3ab26e29177b8889b6e76ec759901a73b41
sugyan <sugi1982@gmail.com>
2021-02-27 22:57:04 +0900

    Add february-leetcoding-challenge-2021/week-4/3654

---
+++ others/february-leetcoding-challenge-2021/week-4/3654/lib.rs
@@ -0,0 +1,49 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn divide(dividend: i32, divisor: i32) -> i32 {
+        let negative = (dividend < 0) != (divisor < 0);
+        let (mut dividend, mut divisor) = (i64::from(dividend).abs(), i64::from(divisor).abs());
+        let mut ret = 0_i64;
+        let mut n = 1;
+        while (divisor << 1) <= dividend {
+            divisor <<= 1;
+            n <<= 1;
+        }
+        while n > 0 {
+            if dividend >= divisor {
+                ret += n;
+                dividend -= divisor;
+            }
+            divisor >>= 1;
+            n >>= 1;
+        }
+        let answer = if negative { -ret } else { ret };
+        answer.min(i64::from(std::i32::MAX)) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(3, Solution::divide(10, 3));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(-2, Solution::divide(7, -3));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(0, Solution::divide(0, 1));
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(1, Solution::divide(1, 1));
+    }
+}
