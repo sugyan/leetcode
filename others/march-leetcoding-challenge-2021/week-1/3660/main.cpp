a743d3eb211f731c4705cfcdc8a849384e9112a4
sugyan <sugi1982@gmail.com>
2021-03-04 17:54:38 +0900

    Update march-leetcoding-challenge-2021/week-1/3660

--- others/march-leetcoding-challenge-2021/week-1/3660/main.cpp
+++ others/march-leetcoding-challenge-2021/week-1/3660/main.cpp
@@ -13,24 +13,10 @@ struct ListNode {
 class Solution {
    public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int lengthA = 0, lengthB = 0;
-        for (ListNode *node = headA; node != nullptr; node = node->next)
-            ++lengthA;
-        for (ListNode *node = headB; node != nullptr; node = node->next)
-            ++lengthB;
-
         ListNode *nodeA = headA, *nodeB = headB;
-        if (lengthA > lengthB) {
-            for (int i = 0; nodeA != nullptr && i < lengthA - lengthB; ++i)
-                nodeA = nodeA->next;
-        } else {
-            for (int i = 0; nodeB != nullptr && i < lengthB - lengthA; ++i)
-                nodeB = nodeB->next;
-        }
-
-        while (nodeA != nullptr && nodeB != nullptr && nodeA != nodeB) {
-            nodeA = nodeA->next;
-            nodeB = nodeB->next;
+        while (nodeA != nodeB) {
+            nodeA = nodeA != nullptr ? nodeA->next : headB;
+            nodeB = nodeB != nullptr ? nodeB->next : headA;
         }
         return nodeA;
     }
