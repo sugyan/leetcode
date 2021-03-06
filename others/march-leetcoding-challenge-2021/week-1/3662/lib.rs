c7f39d9e50326fcd9ec2b0d5a6b86b4a98d50dbb
sugyan <sugi1982@gmail.com>
2021-03-07 00:06:11 +0900

    Update march-leetcoding-challenge-2021/week-1/3662 (but slow...)

--- others/march-leetcoding-challenge-2021/week-1/3662/lib.rs
+++ others/march-leetcoding-challenge-2021/week-1/3662/lib.rs
@@ -1,19 +1,34 @@
 pub struct Solution;
 
+#[derive(Default)]
+struct Trie {
+    children: [Option<Box<Trie>>; 26],
+}
+
 impl Solution {
     pub fn minimum_length_encoding(words: Vec<String>) -> i32 {
-        let mut v = words
-            .iter()
-            .map(|s| s.chars().rev().collect::<String>())
-            .collect::<Vec<_>>();
-        v.sort_unstable();
-        let mut answer = 0;
-        for (i, word) in v.iter().enumerate() {
-            if i + 1 >= v.len() || !v[i + 1].starts_with(&v[i]) {
-                answer += word.len() + 1;
+        let mut trie = Trie::default();
+        for word in &words {
+            let mut node = &mut trie;
+            for &u in word.as_bytes().iter().rev() {
+                node = node.children[(u - b'a') as usize].get_or_insert_with(Default::default);
             }
         }
-        answer as i32
+        Self::dfs(&trie, 0) as i32
+    }
+    fn dfs(t: &Trie, depth: usize) -> usize {
+        let (mut ret, mut end) = (0, true);
+        for child in &t.children {
+            if let Some(c) = child {
+                end = false;
+                ret += Self::dfs(&c, depth + 1);
+            }
+        }
+        if end {
+            depth + 1
+        } else {
+            ret
+        }
     }
 }
 
