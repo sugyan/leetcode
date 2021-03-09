d8d13c249aeca070fd22584529a1270bf1289323
sugyan <sugi1982@gmail.com>
2021-03-09 18:24:09 +0900

    Add march-leetcoding-challenge-2021/week-2/3666

---
+++ others/march-leetcoding-challenge-2021/week-2/3666/lib.rs
@@ -0,0 +1,102 @@
+use std::cell::RefCell;
+use std::cmp::Ordering;
+use std::rc::Rc;
+use utils::TreeNode;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn add_one_row(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        v: i32,
+        d: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if d == 1 {
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: v,
+                left: root,
+                right: None,
+            })))
+        } else {
+            Self::dfs(&root, v, d - 2);
+            root
+        }
+    }
+    fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, v: i32, d: i32) {
+        if let Some(n) = node {
+            match d.cmp(&0) {
+                Ordering::Less => {}
+                Ordering::Equal => {
+                    let left = n.borrow().left.clone();
+                    let right = n.borrow().right.clone();
+                    let mut n = n.borrow_mut();
+                    n.left = Some(Rc::new(RefCell::new(TreeNode {
+                        val: v,
+                        left,
+                        right: None,
+                    })));
+                    n.right = Some(Rc::new(RefCell::new(TreeNode {
+                        val: v,
+                        left: None,
+                        right,
+                    })));
+                }
+                Ordering::Greater => {
+                    Self::dfs(&n.borrow().left, v, d - 1);
+                    Self::dfs(&n.borrow().right, v, d - 1);
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use utils::to_tree;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            to_tree(vec![
+                Some(4),
+                Some(1),
+                Some(1),
+                Some(2),
+                None,
+                None,
+                Some(6),
+                Some(3),
+                Some(1),
+                Some(5)
+            ]),
+            Solution::add_one_row(
+                to_tree(vec![Some(4), Some(2), Some(6), Some(3), Some(1), Some(5)]),
+                1,
+                2
+            )
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            to_tree(vec![
+                Some(4),
+                Some(2),
+                None,
+                Some(1),
+                Some(1),
+                Some(3),
+                None,
+                None,
+                Some(1),
+            ]),
+            Solution::add_one_row(
+                to_tree(vec![Some(4), Some(2), None, Some(3), Some(1)]),
+                1,
+                3
+            )
+        );
+    }
+}
