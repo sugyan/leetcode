31b2c4fdb8e900b23c3eeb050debc06b7c5baeae
sugyan <sugi1982@gmail.com>
2021-03-12 17:51:58 +0900

    Update march-leetcoding-challenge-2021/week-2/3669 (8ms -> 0ms)

--- others/march-leetcoding-challenge-2021/week-2/3669/lib.rs
+++ others/march-leetcoding-challenge-2021/week-2/3669/lib.rs
@@ -4,18 +4,20 @@ impl Solution {
     pub fn has_all_codes(s: String, k: i32) -> bool {
         let k = k as usize;
         let mut v = vec![false; 1 << k];
-        let mut n = 0;
+        let (mut n, mut count) = (0, 1 << k);
         let mask = (1 << k) - 1;
         for (i, &b) in s.as_bytes().iter().enumerate() {
-            n <<= 1;
-            n += if b == b'1' { 1 } else { 0 };
-            n &= mask;
-            if i + 1 < k {
+            n = ((n << 1) & mask) + if b == b'1' { 1 } else { 0 };
+            if i + 1 < k || v[n] {
                 continue;
             }
             v[n] = true;
+            count -= 1;
+            if count == 0 {
+                return true;
+            }
         }
-        v.iter().all(|&b| b)
+        false
     }
 }
 
