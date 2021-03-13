f0c7cca56854b11ffe2c93e1bb1f72f457dd10ab
sugyan <sugi1982@gmail.com>
2021-03-14 00:15:47 +0900

    Update march-leetcoding-challenge-2021/week-2/3670

--- others/march-leetcoding-challenge-2021/week-2/3670/lib.rs
+++ others/march-leetcoding-challenge-2021/week-2/3670/lib.rs
@@ -2,29 +2,26 @@ use std::collections::HashMap;
 
 pub struct Solution;
 
-const DIV: i64 = 1_000_000_007;
+const MOD: i64 = 1_000_000_007;
 
 impl Solution {
     pub fn num_factored_binary_trees(arr: Vec<i32>) -> i32 {
-        let mut hm = arr
-            .iter()
-            .map(|&n| (i64::from(n), 1))
-            .collect::<HashMap<_, _>>();
-        let mut arr = arr.into_iter().map(i64::from).collect::<Vec<_>>();
+        let mut hm = arr.iter().map(|&n| (n, 1)).collect::<HashMap<_, _>>();
+        let mut arr = arr;
         arr.sort_unstable();
         for i in 1..arr.len() {
-            for j in (0..i).filter(|&j| arr[j] * arr[j] <= arr[i] && arr[i] % arr[j] == 0) {
-                if let Some(&v) = hm.get(&(arr[i] / arr[j])) {
-                    let n = (*hm.get(&arr[j]).unwrap()
-                        * v
-                        * if arr[j] * arr[j] != arr[i] { 2 } else { 1 })
-                        % DIV;
-                    *hm.entry(arr[i]).or_default() += n;
+            for j in 0..i {
+                if arr[i] % arr[j] == 0 {
+                    if let Some(&v) = hm.get(&(arr[i] / arr[j])) {
+                        let vj = *hm.get_mut(&arr[j]).unwrap();
+                        if let Some(vi) = hm.get_mut(&arr[i]) {
+                            *vi = (*vi + vj * v) % MOD
+                        }
+                    }
                 }
             }
         }
-        hm.values()
-            .fold(0, |acc, &x| ((i64::from(acc) + x) % DIV) as i32)
+        (hm.values().sum::<i64>() % MOD) as i32
     }
 }
 
