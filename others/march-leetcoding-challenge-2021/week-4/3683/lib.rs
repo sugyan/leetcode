e7ba194597a12aa55e1f3c511d11e5c37dca814c
sugyan <sugi1982@gmail.com>
2021-03-24 22:43:51 +0900

    Update march-leetcoding-challenge-2021/week-4/3683

--- others/march-leetcoding-challenge-2021/week-4/3683/lib.rs
+++ others/march-leetcoding-challenge-2021/week-4/3683/lib.rs
@@ -1,34 +1,31 @@
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::HashMap;
 
 pub struct Solution;
 
 impl Solution {
     pub fn advantage_count(a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
-        let mut bh = a.iter().map(Reverse).collect::<BinaryHeap<_>>();
-        let mut b = b
-            .iter()
-            .enumerate()
-            .map(|(i, &n)| (n, i))
-            .collect::<Vec<_>>();
-        b.sort_unstable();
-        let mut answer = vec![-1; a.len()];
-        let mut fill = Vec::new();
-        for &(n, i) in &b {
-            while let Some(Reverse(&m)) = bh.pop() {
-                if m > n {
-                    answer[i] = m;
-                    break;
-                }
-                fill.push(m);
+        let mut a_sorted = a;
+        let mut b_sorted = b.clone();
+        a_sorted.sort_unstable();
+        b_sorted.sort_unstable();
+        let mut hm: HashMap<_, Vec<_>> = HashMap::new();
+        let mut remain = Vec::new();
+        let mut i = 0;
+        for &n in &a_sorted {
+            if n > b_sorted[i] {
+                hm.entry(b_sorted[i]).or_default().push(n);
+                i += 1;
+            } else {
+                remain.push(n);
             }
         }
-        for n in answer.iter_mut() {
-            if *n == -1 {
-                *n = fill.pop().unwrap();
-            }
-        }
-        answer
+        b.into_iter()
+            .filter_map(|n| {
+                hm.get_mut(&n)
+                    .filter(|v| !v.is_empty())
+                    .map_or_else(|| remain.pop(), |v| v.pop())
+            })
+            .collect()
     }
 }
 
@@ -38,29 +35,23 @@ mod tests {
 
     #[test]
     fn example_1() {
-        let mut ret = Solution::advantage_count(vec![2, 7, 11, 15], vec![1, 10, 4, 11]);
-        assert_eq!(
-            4,
-            ret.iter()
-                .zip(vec![1, 10, 4, 11].iter())
-                .filter(|(a, b)| a > b)
-                .count()
-        );
+        let mut a = vec![2, 7, 11, 15];
+        let b = vec![1, 10, 4, 11];
+        let mut ret = Solution::advantage_count(a.clone(), b.clone());
+        assert_eq!(4, ret.iter().zip(b.iter()).filter(|(a, b)| a > b).count());
         ret.sort_unstable();
-        assert_eq!(vec![2, 7, 11, 15], ret);
+        a.sort_unstable();
+        assert_eq!(ret, a);
     }
 
     #[test]
     fn example_2() {
-        let mut ret = Solution::advantage_count(vec![12, 24, 8, 32], vec![13, 25, 32, 11]);
-        assert_eq!(
-            3,
-            ret.iter()
-                .zip(vec![13, 25, 32, 11].iter())
-                .filter(|(a, b)| a > b)
-                .count()
-        );
+        let mut a = vec![12, 24, 8, 32];
+        let b = vec![13, 25, 32, 11];
+        let mut ret = Solution::advantage_count(a.clone(), b.clone());
+        assert_eq!(3, ret.iter().zip(b.iter()).filter(|(a, b)| a > b).count());
         ret.sort_unstable();
-        assert_eq!(vec![8, 12, 24, 32], ret);
+        a.sort_unstable();
+        assert_eq!(ret, a);
     }
 }
