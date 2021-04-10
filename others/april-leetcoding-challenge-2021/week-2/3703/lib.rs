87a89d7fdf0bf0b555cbc4f1428212fc127471b0
sugyan <sugi1982@gmail.com>
2021-04-10 23:01:14 +0900

    Update april-leetcoding-challenge-2021/week-2/3703 (72ms -> 12ms)

--- others/april-leetcoding-challenge-2021/week-2/3703/lib.rs
+++ others/april-leetcoding-challenge-2021/week-2/3703/lib.rs
@@ -1,46 +1,30 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 
 impl Solution {
     pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
         let (m, n) = (matrix.len(), matrix[0].len());
         let mut cache = vec![vec![None; n]; m];
-        let mut graph = HashMap::new();
-        for i in 0..m {
-            for j in 0..n {
-                for d in [0, -1, 0, 1, 0].windows(2) {
-                    let (ni, nj) = (i as i32 + d[0], j as i32 + d[1]);
-                    if (0..m as i32).contains(&ni) && (0..n as i32).contains(&nj) {
-                        let (ni, nj) = (ni as usize, nj as usize);
-                        if matrix[ni][nj] > matrix[i][j] {
-                            graph.entry((i, j)).or_insert_with(Vec::new).push((ni, nj));
-                        }
-                    }
-                }
-            }
-        }
         let mut answer = 0;
         for i in 0..m {
             for j in 0..n {
-                answer = answer.max(Self::dfs(&graph, &mut cache, i, j));
+                answer = answer.max(Self::dfs(&matrix, &mut cache, i, j));
             }
         }
         answer
     }
-    fn dfs(
-        graph: &HashMap<(usize, usize), Vec<(usize, usize)>>,
-        cache: &mut Vec<Vec<Option<i32>>>,
-        i: usize,
-        j: usize,
-    ) -> i32 {
+    fn dfs(matrix: &[Vec<i32>], cache: &mut Vec<Vec<Option<i32>>>, i: usize, j: usize) -> i32 {
         if let Some(c) = cache[i][j] {
             return c;
         }
+        let (m, n) = (matrix.len(), matrix[0].len());
         let mut ret = 1;
-        if let Some(v) = graph.get(&(i, j)) {
-            for &p in v {
-                ret = ret.max(Self::dfs(graph, cache, p.0, p.1) + 1);
+        for d in [0, -1, 0, 1, 0].windows(2) {
+            let (ni, nj) = (i as i32 + d[0], j as i32 + d[1]);
+            if (0..m as i32).contains(&ni) && (0..n as i32).contains(&nj) {
+                let (ni, nj) = (ni as usize, nj as usize);
+                if matrix[ni][nj] > matrix[i][j] {
+                    ret = ret.max(Self::dfs(matrix, cache, ni, nj) + 1);
+                }
             }
         }
         cache[i][j] = Some(ret);
