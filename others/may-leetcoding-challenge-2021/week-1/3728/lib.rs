7a91f97162904fa099f63c9581044cb9392b3d6b
sugyan <sugi1982@gmail.com>
2021-05-01 19:36:47 +0900

    Update may-leetcoding-challenge-2021/week-1/3728 (144ms -> 136ms)

--- others/may-leetcoding-challenge-2021/week-1/3728/lib.rs
+++ others/may-leetcoding-challenge-2021/week-1/3728/lib.rs
@@ -1,7 +1,11 @@
-use std::collections::HashMap;
+#[derive(Default)]
+struct Trie {
+    index: i32,
+    childlen: [Option<Box<Trie>>; 27],
+}
 
 pub struct WordFilter {
-    index_map: HashMap<String, usize>,
+    trie: Trie,
 }
 
 /**
@@ -10,29 +14,31 @@ pub struct WordFilter {
  */
 impl WordFilter {
     pub fn new(words: Vec<String>) -> Self {
-        let mut index_map = HashMap::new();
+        let mut trie = Trie::default();
         for (i, word) in words.iter().enumerate() {
             for j in 0..word.len() {
-                let prefix = &word[..=j];
-                for k in 0..word.len() {
-                    index_map.insert(
-                        String::new() + prefix + "#" + &word[word.len() - 1 - k..],
-                        i,
-                    );
+                let s = String::new() + &word[word.len() - 1 - j..] + "{" + &word;
+                let mut node = &mut trie;
+                for &b in s.as_bytes() {
+                    node = node.childlen[(b - b'a') as usize].get_or_insert_with(Default::default);
+                    node.index = i as i32;
                 }
             }
         }
-        Self { index_map }
+        Self { trie }
     }
 
     pub fn f(&self, prefix: String, suffix: String) -> i32 {
-        if let Some(&i) = self
-            .index_map
-            .get(&(String::new() + &prefix + "#" + &suffix))
-        {
-            return i as i32;
+        let mut node = &self.trie;
+        let s = String::new() + &suffix + "{" + &prefix;
+        for &b in s.as_bytes() {
+            if let Some(n) = &node.childlen[(b - b'a') as usize] {
+                node = n.as_ref();
+            } else {
+                return -1;
+            }
         }
-        -1
+        node.index
     }
 }
 
