5a5d598161e185f8124a6142055d28f1a76af86e
sugyan <sugi1982@gmail.com>
2021-05-07 17:16:18 +0900

    Add may-leetcoding-challenge-2021/week-1/3734

---
+++ others/may-leetcoding-challenge-2021/week-1/3734/lib.rs
@@ -0,0 +1,39 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_distance(word1: String, word2: String) -> i32 {
+        let s1 = word1.as_bytes();
+        let s2 = word2.as_bytes();
+        let mut dp = vec![vec![0; s2.len() + 1]; s1.len() + 1];
+        for (i, b1) in s1.iter().enumerate() {
+            for (j, b2) in s2.iter().enumerate() {
+                dp[i + 1][j + 1] = dp[i + 1][j].max(dp[i][j + 1]);
+                if b1 == b2 {
+                    dp[i + 1][j + 1] = dp[i + 1][j + 1].max(dp[i][j] + 1);
+                }
+            }
+        }
+        (s1.len() + s2.len() - 2 * dp[s1.len()][s2.len()]) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            2,
+            Solution::min_distance(String::from("sea"), String::from("eat"))
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            4,
+            Solution::min_distance(String::from("leetcode"), String::from("etco"))
+        );
+    }
+}
