0271ee7b26f6869aecb73de42d5184b6ba297ba2
sugyan <sugi1982@gmail.com>
2021-05-07 17:28:04 +0900

    Update may-leetcoding-challenge-2021/week-1/3734 (4.2MB -> 2.1MB)

--- others/may-leetcoding-challenge-2021/week-1/3734/lib.rs
+++ others/may-leetcoding-challenge-2021/week-1/3734/lib.rs
@@ -2,19 +2,17 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        let s1 = word1.as_bytes();
         let s2 = word2.as_bytes();
-        let mut dp = vec![vec![0; s2.len() + 1]; s1.len() + 1];
-        for (i, b1) in s1.iter().enumerate() {
+        let mut dp = vec![0; s2.len() + 1];
+        for b1 in word1.as_bytes() {
+            let mut prev = dp[0];
             for (j, b2) in s2.iter().enumerate() {
-                dp[i + 1][j + 1] = if b1 == b2 {
-                    dp[i + 1][j + 1].max(dp[i][j] + 1)
-                } else {
-                    dp[i + 1][j].max(dp[i][j + 1])
-                }
+                let val = dp[j + 1].max(if b1 == b2 { prev + 1 } else { dp[j] });
+                prev = dp[j + 1];
+                dp[j + 1] = val;
             }
         }
-        (s1.len() + s2.len() - 2 * dp[s1.len()][s2.len()]) as i32
+        (word1.len() + word2.len() - 2 * dp[s2.len()]) as i32
     }
 }
 
