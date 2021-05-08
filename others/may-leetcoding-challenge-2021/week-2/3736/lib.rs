2986d9470fc913db1265217715d2820699fb7d23
sugyan <sugi1982@gmail.com>
2021-05-08 18:56:54 +0900

    Update may-leetcoding-challenge-2021/week-2/3736

--- others/may-leetcoding-challenge-2021/week-2/3736/lib.rs
+++ others/may-leetcoding-challenge-2021/week-2/3736/lib.rs
@@ -1,43 +1,47 @@
+use std::ops::RangeInclusive;
+
 pub struct Solution;
 
 impl Solution {
     pub fn superpalindromes_in_range(left: String, right: String) -> i32 {
         let mut answer = 0;
         if let (Ok(l), Ok(r)) = (left.parse::<u64>(), right.parse::<u64>()) {
-            for i in 1..=right.len() {
-                let mut candidates = Vec::new();
-                let mut v = Vec::new();
-                Self::backtrack(&mut candidates, &mut v, i, 0);
-                for &c in &candidates {
-                    if c > r {
-                        return answer;
-                    }
-                    if l <= c && c <= r {
-                        answer += 1;
-                    }
-                }
+            {
+                let mut v = Vec::with_capacity(6);
+                Self::backtrack(&mut answer, &mut v, &(l..=r), false);
+            }
+            {
+                let mut v = Vec::with_capacity(6);
+                Self::backtrack(&mut answer, &mut v, &(l..=r), true);
             }
         }
         answer
     }
-    fn backtrack(candidates: &mut Vec<u64>, v: &mut Vec<u8>, len: usize, sum: u8) {
-        if v.len() == (len + 1) / 2 {
-            println!("{:?} ({})", v, len);
-            let candidate = v.iter().rev().skip(if len % 2 != 0 { 1 } else { 0 }).fold(
-                v.iter().fold(0, |acc, &u| acc * 10 + u as u64),
-                |acc, &u| acc * 10 + u as u64,
-            );
-            candidates.push(candidate * candidate);
-            return;
-        }
-        for u in 0..=9 {
-            if v.is_empty() && u == 0 {
-                continue;
+    fn backtrack(answer: &mut i32, v: &mut Vec<u64>, range: &RangeInclusive<u64>, odd: bool) {
+        if v.len() == 6 {
+            let digits = if let Some(pos) = v.iter().position(|&d| d != 0) {
+                &v[pos..]
+            } else {
+                &v
+            };
+            let mut n = 0;
+            for &d in digits.iter() {
+                n = n * 10 + d;
             }
-            if sum * 2 + u * u * if len % 2 == 0 { 2 } else { 1 } < 10 {
-                v.push(u);
-                Self::backtrack(candidates, v, len, sum + u * u);
-                v.pop();
+            for &d in digits.iter().rev().skip(if odd { 1 } else { 0 }) {
+                n = n * 10 + d;
+            }
+            if range.contains(&(n.saturating_mul(n))) {
+                *answer += 1;
+            }
+        } else {
+            let sum = v.iter().map(|d| d * d).sum::<u64>();
+            for u in 0..=9 {
+                if sum * 2 + u * u * if odd { 1 } else { 2 } < 10 {
+                    v.push(u);
+                    Self::backtrack(answer, v, range, odd);
+                    v.pop();
+                }
             }
         }
     }
