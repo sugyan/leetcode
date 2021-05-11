6427f39a0768aa2d292db698168b8828cd18255d
sugyan <sugi1982@gmail.com>
2021-05-11 23:25:50 +0900

    Add may-leetcoding-challenge-2021/week-2/3739

---
+++ others/may-leetcoding-challenge-2021/week-2/3739/lib.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn max_score(card_points: Vec<i32>, k: i32) -> i32 {
+        let mut sum = card_points[0..k as usize].iter().sum::<i32>();
+        let mut answer = sum;
+        for i in 0..k as usize {
+            sum -= card_points[k as usize - 1 - i];
+            sum += card_points[card_points.len() - 1 - i];
+            answer = answer.max(sum);
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(12, Solution::max_score(vec![1, 2, 3, 4, 5, 6, 1], 3));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(4, Solution::max_score(vec![2, 2, 2], 2));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(55, Solution::max_score(vec![9, 7, 7, 9, 7, 7, 9], 7));
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(1, Solution::max_score(vec![1, 1000, 1], 1));
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(
+            202,
+            Solution::max_score(vec![1, 79, 80, 1, 1, 1, 200, 1], 3)
+        );
+    }
+}
