b456b90ecc28101936e6fd1c55c243dbd833eedc
sugyan <sugi1982@gmail.com>
2021-05-14 00:35:42 +0900

    Update may-leetcoding-challenge-2021/week-2/3741

--- others/may-leetcoding-challenge-2021/week-2/3741/lib.rs
+++ others/may-leetcoding-challenge-2021/week-2/3741/lib.rs
@@ -2,38 +2,38 @@ pub struct Solution;
 
 impl Solution {
     pub fn ambiguous_coordinates(s: String) -> Vec<String> {
-        let v = s.as_bytes()[1..s.len() - 1]
-            .iter()
-            .map(|&u| u - b'0')
-            .collect::<Vec<_>>();
+        let s = s.chars().skip(1).take(s.len() - 2).collect::<Vec<_>>();
+        let candidates = |v: &[char]| -> Vec<String> {
+            (0..v.len())
+                .filter_map(|i| {
+                    let s: String = if i == 0 {
+                        v.iter().collect()
+                    } else {
+                        v[..i]
+                            .iter()
+                            .chain(std::iter::once(&'.'))
+                            .chain(v[i..].iter())
+                            .collect()
+                    };
+                    if (s != "0" && s.starts_with('0') && !s.starts_with("0."))
+                        || (s.contains('.') && s.ends_with('0'))
+                    {
+                        None
+                    } else {
+                        Some(s)
+                    }
+                })
+                .collect()
+        };
         let mut answer = Vec::new();
-        for i in 1..v.len() {
-            let (mut vx, mut vy) = (Vec::new(), Vec::new());
-            Self::dfs(&mut vx, &v[..i], String::new());
-            Self::dfs(&mut vy, &v[i..], String::new());
-            for x in &vx {
-                for y in &vy {
+        for i in 1..s.len() {
+            for x in &candidates(&s[..i]) {
+                for y in &candidates(&s[i..]) {
                     answer.push(format!("({}, {})", x, y));
                 }
             }
         }
         answer
-    }
-    fn dfs(ret: &mut Vec<String>, v: &[u8], s: String) {
-        if let Some(&f) = v.first() {
-            Self::dfs(ret, &v[1..], s.clone() + &f.to_string());
-            if !(s.is_empty() || s.contains('.')) {
-                Self::dfs(ret, &v[1..], s + "." + &f.to_string());
-            }
-        } else {
-            if s != "0" && s.starts_with('0') && !s.starts_with("0.") {
-                return;
-            }
-            if s.contains('.') && s.ends_with('0') {
-                return;
-            }
-            ret.push(s);
-        }
     }
 }
 
