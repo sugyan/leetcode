a9b47486aa9c3c4b1470a8c8f73691a72bcb36dc
sugyan <sugi1982@gmail.com>
2021-06-12 01:47:46 +0900

    Update june-leetcoding-challenge-2021/week-2/3775

--- others/june-leetcoding-challenge-2021/week-2/3775/lib.rs
+++ others/june-leetcoding-challenge-2021/week-2/3775/lib.rs
@@ -3,19 +3,17 @@ pub struct Solution;
 impl Solution {
     pub fn stone_game_vii(stones: Vec<i32>) -> i32 {
         let mut dp = vec![vec![0; stones.len()]; stones.len()];
-        if stones.len() % 2 == 0 {
-            for (i, &s) in stones.iter().enumerate() {
-                dp[i][i] = s;
-            }
+        let mut sums = vec![0; stones.len() + 1];
+        for (i, &s) in stones.iter().enumerate() {
+            sums[i + 1] = sums[i] + s;
         }
-        for i in 0..stones.len() - 1 {
-            for j in 0..stones.len() - i - 1 {
-                let k = j + i + 1;
-                dp[j][k] = if i % 2 != stones.len() % 2 {
-                    (dp[j][k - 1] + stones[k]).min(dp[j + 1][k] + stones[j])
-                } else {
-                    dp[j][k - 1].max(dp[j + 1][k])
-                };
+        for i in 1..stones.len() {
+            for j in 0..stones.len() - i {
+                let k = i + j;
+                dp[j][k] = std::cmp::max(
+                    sums[k + 1] - sums[j + 1] - dp[j + 1][k],
+                    sums[k] - sums[j] - dp[j][k - 1],
+                );
             }
         }
         dp[0][stones.len() - 1]
