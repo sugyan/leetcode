1dd75529bb807f69b3793db24457c1e6845eef59
sugyan <sugi1982@gmail.com>
2021-06-21 16:39:20 +0900

    Add june-leetcoding-challenge-2021/week-3/3786

---
+++ others/june-leetcoding-challenge-2021/week-3/3786/lib.rs
@@ -0,0 +1,39 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        (0..num_rows)
+            .scan(Vec::with_capacity(num_rows as usize), |state, _| {
+                for i in (1..state.len()).rev() {
+                    state[i] += state[i - 1];
+                }
+                state.push(1);
+                Some(state.clone())
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            vec![
+                vec![1],
+                vec![1, 1],
+                vec![1, 2, 1],
+                vec![1, 3, 3, 1],
+                vec![1, 4, 6, 4, 1]
+            ],
+            Solution::generate(5)
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(vec![vec![1]], Solution::generate(1));
+    }
+}
