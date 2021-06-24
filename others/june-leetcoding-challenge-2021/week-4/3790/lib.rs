c8b37f35044b2efab665a1c3689c5a7d8d508c7f
sugyan <sugi1982@gmail.com>
2021-06-25 00:44:06 +0900

    Update june-leetcoding-challenge-2021/week-4/3790

--- others/june-leetcoding-challenge-2021/week-4/3790/lib.rs
+++ others/june-leetcoding-challenge-2021/week-4/3790/lib.rs
@@ -1,36 +1,39 @@
-use std::collections::HashMap;
-
 const MOD: i32 = 1_000_000_007;
 
 pub struct Solution;
 
 impl Solution {
     pub fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32 {
-        let mut grid = HashMap::new();
-        grid.insert((start_row, start_column), 1);
+        let (m, n) = (m as usize, n as usize);
+        let mut dp = vec![vec![0; n]; m];
+        dp[start_row as usize][start_column as usize] = 1;
         let mut answer = 0;
         for _ in 0..max_move {
-            for ((row, col), k) in grid.drain().collect::<Vec<_>>() {
-                if row == 0 {
-                    answer = (answer + k) % MOD
-                }
-                if row == m - 1 {
-                    answer = (answer + k) % MOD
-                }
-                if col == 0 {
-                    answer = (answer + k) % MOD
-                }
-                if col == n - 1 {
-                    answer = (answer + k) % MOD
-                }
-                for d in [0, 1, 0, -1, 0].windows(2) {
-                    let (row, col) = (row + d[0], col + d[1]);
-                    if (0..m).contains(&row) && (0..n).contains(&col) {
-                        let v = grid.entry((row, col)).or_default();
-                        *v = (*v + k) % MOD;
+            let mut tmp = vec![vec![0; n]; m];
+            for i in 0..m {
+                for j in 0..n {
+                    if i == 0 {
+                        answer = (answer + dp[i][j]) % MOD;
+                    }
+                    if i == m - 1 {
+                        answer = (answer + dp[i][j]) % MOD;
+                    }
+                    if j == 0 {
+                        answer = (answer + dp[i][j]) % MOD;
+                    }
+                    if j == n - 1 {
+                        answer = (answer + dp[i][j]) % MOD;
+                    }
+                    for d in [0, 1, 0, !0, 0].windows(2) {
+                        let row = i.wrapping_add(d[0]);
+                        let col = j.wrapping_add(d[1]);
+                        if (0..m).contains(&row) && (0..n).contains(&col) {
+                            tmp[row][col] = (tmp[row][col] + dp[i][j]) % MOD;
+                        }
                     }
                 }
             }
+            dp = tmp;
         }
         answer
     }
