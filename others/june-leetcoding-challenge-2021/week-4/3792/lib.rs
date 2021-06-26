5b67b43ee5c6dedcae531b3b8d54868c463b738d
sugyan <sugi1982@gmail.com>
2021-06-27 01:05:38 +0900

    Update june-leetcoding-challenge-2021/week-4/3792 (1344ms -> 64ms)

--- others/june-leetcoding-challenge-2021/week-4/3792/lib.rs
+++ others/june-leetcoding-challenge-2021/week-4/3792/lib.rs
@@ -1,23 +1,44 @@
-use std::cmp::Ordering;
+struct BinaryIndexedTree {
+    n: i32,
+    v: Vec<i32>,
+}
+
+impl BinaryIndexedTree {
+    fn new(n: i32) -> Self {
+        Self {
+            n,
+            v: vec![0; n as usize + 1],
+        }
+    }
+    fn add(&mut self, i: i32) {
+        let mut x = i;
+        while x <= self.n {
+            self.v[x as usize] += 1;
+            x += 1 << x.trailing_zeros();
+        }
+    }
+    fn sum(&self, i: i32) -> i32 {
+        let mut ret = 0;
+        let mut x = i;
+        while x > 0 {
+            ret += self.v[x as usize];
+            x -= 1 << x.trailing_zeros();
+        }
+        ret
+    }
+}
 
 pub struct Solution;
 
+const BASE: i32 = 10_000;
+
 impl Solution {
     pub fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
-        let mut v = Vec::with_capacity(nums.len());
+        let mut bit = BinaryIndexedTree::new(BASE * 2 + 1);
         let mut answer = vec![0; nums.len()];
-        for (i, num) in nums.iter().enumerate().rev() {
-            let j = v
-                .binary_search_by(|&probe| {
-                    if probe >= num {
-                        Ordering::Greater
-                    } else {
-                        Ordering::Less
-                    }
-                })
-                .unwrap_err();
-            answer[i] = j as i32;
-            v.insert(j, num);
+        for (i, &num) in nums.iter().enumerate().rev() {
+            answer[i] = bit.sum(BASE + num);
+            bit.add(BASE + num + 1);
         }
         answer
     }
