442f5324ac015c79a5e7f71cbd5deeb481c82405
sugyan <sugi1982@gmail.com>
2021-07-10 00:24:09 +0900

    Add july-leetcoding-challenge-2021/week-2/3808

---
+++ others/july-leetcoding-challenge-2021/week-2/3808/lib.rs
@@ -0,0 +1,36 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .fold(Vec::new(), |mut v, x| {
+                match v.binary_search(x) {
+                    Ok(i) => v[i] = *x,
+                    Err(i) if i < v.len() => v[i] = *x,
+                    Err(_) => v.push(*x),
+                };
+                v
+            })
+            .len() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(4, Solution::length_of_lis(vec![10, 9, 2, 5, 3, 7, 101, 18]));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(4, Solution::length_of_lis(vec![0, 1, 0, 3, 2, 3]));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(1, Solution::length_of_lis(vec![7, 7, 7, 7, 7, 7, 7]));
+    }
+}
