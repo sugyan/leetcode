1d7e8f82563254bcae9706132b3b8de3fbbca366
sugyan <sugi1982@gmail.com>
2021-07-12 00:00:21 +0900

    Update july-leetcoding-challenge-2021/week-2/3810

--- others/july-leetcoding-challenge-2021/week-2/3810/lib.rs
+++ others/july-leetcoding-challenge-2021/week-2/3810/lib.rs
@@ -1,6 +1,11 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 #[derive(Default)]
 pub struct MedianFinder {
-    v: Vec<i32>,
+    small: BinaryHeap<i32>,
+    large: BinaryHeap<Reverse<i32>>,
+    odd: bool,
 }
 
 /**
@@ -14,20 +19,25 @@ impl MedianFinder {
     }
 
     pub fn add_num(&mut self, num: i32) {
-        self.v.insert(
-            match self.v.binary_search(&num) {
-                Ok(i) => i,
-                Err(i) => i,
-            },
-            num,
-        );
+        if self.odd {
+            self.small.push(num);
+            if let Some(max) = self.small.pop() {
+                self.large.push(Reverse(max));
+            }
+        } else {
+            self.large.push(Reverse(num));
+            if let Some(Reverse(min)) = self.large.pop() {
+                self.small.push(min);
+            }
+        }
+        self.odd = !self.odd;
     }
 
     pub fn find_median(&self) -> f64 {
-        if self.v.len() % 2 != 0 {
-            self.v[self.v.len() / 2] as f64
+        if self.odd {
+            *self.small.peek().unwrap() as f64
         } else {
-            (self.v[self.v.len() / 2] + self.v[self.v.len() / 2 - 1]) as f64 / 2.0
+            (self.small.peek().unwrap() + self.large.peek().unwrap().0) as f64 / 2.0
         }
     }
 }
