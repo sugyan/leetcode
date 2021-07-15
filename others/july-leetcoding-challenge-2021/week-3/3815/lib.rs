aae09e287e58e0833866fabdb88ec1b4e5456f95
sugyan <sugi1982@gmail.com>
2021-07-15 23:20:52 +0900

    Update july-leetcoding-challenge-2021/week-3/3815 (708ms -> 28ms)

--- others/july-leetcoding-challenge-2021/week-3/3815/lib.rs
+++ others/july-leetcoding-challenge-2021/week-3/3815/lib.rs
@@ -1,29 +1,20 @@
-use std::cmp::Ordering;
-
 pub struct Solution;
 
 impl Solution {
     pub fn triangle_number(nums: Vec<i32>) -> i32 {
+        let mut nums = nums.iter().filter(|&n| *n > 0).collect::<Vec<_>>();
         if nums.len() < 3 {
             return 0;
         }
-        let mut nums = nums;
         nums.sort_unstable();
         let mut answer = 0;
         for i in 0..nums.len() - 2 {
+            let mut k = i + 2;
             for j in i + 1..nums.len() - 1 {
-                if let Err(n) = (j + 1..nums.len())
-                    .collect::<Vec<_>>()
-                    .binary_search_by(|&k| {
-                        if nums[k] < nums[i] + nums[j] {
-                            Ordering::Less
-                        } else {
-                            Ordering::Greater
-                        }
-                    })
-                {
-                    answer += n;
+                while k < nums.len() && nums[i] + nums[j] > *nums[k] {
+                    k += 1;
                 }
+                answer += k - j - 1;
             }
         }
         answer as i32
