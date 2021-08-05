d757a28682e64e4888ba3a3df57ad815b29e1c76
sugyan <sugi1982@gmail.com>
2021-08-06 00:01:33 +0900

    Update august-leetcoding-challenge-2021/week-1/3870

--- others/august-leetcoding-challenge-2021/week-1/3870/lib.rs
+++ others/august-leetcoding-challenge-2021/week-1/3870/lib.rs
@@ -2,18 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn stone_game(piles: Vec<i32>) -> bool {
-        let (mut lo, mut hi) = (0, piles.len() - 1);
-        let mut score = 0;
-        while lo < hi - 2 {
-            if piles[lo] - piles[lo + 1].max(piles[hi]) > piles[hi] - piles[hi - 1].max(piles[lo]) {
-                score += piles[lo];
-                lo += 1;
-            } else {
-                score += piles[hi];
-                hi -= 1;
-            }
-        }
-        score + (piles[lo] - piles[hi]).abs() > 0
+        true
     }
 }
 
