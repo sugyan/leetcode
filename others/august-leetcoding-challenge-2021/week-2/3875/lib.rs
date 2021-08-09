30ba8669f0adc690ab75f235914be7b9264b090b
sugyan <sugi1982@gmail.com>
2021-08-09 16:49:46 +0900

    Update august-leetcoding-challenge-2021/week-2/3875

--- others/august-leetcoding-challenge-2021/week-2/3875/lib.rs
+++ others/august-leetcoding-challenge-2021/week-2/3875/lib.rs
@@ -2,22 +2,21 @@ pub struct Solution;
 
 impl Solution {
     pub fn add_strings(num1: String, num2: String) -> String {
-        let num1 = num1.as_bytes().iter().rev().collect::<Vec<_>>();
-        let num2 = num2.as_bytes().iter().rev().collect::<Vec<_>>();
-        let len = num1.len().max(num2.len()) + 1;
-        let mut v = Vec::with_capacity(len);
+        let mut num1 = num1.bytes().rev();
+        let mut num2 = num2.bytes().rev();
+        let mut v = Vec::with_capacity(num1.len().max(num2.len()) + 1);
         let mut carry = false;
-        for i in 0..len {
-            let d = if carry { 1 } else { 0 }
-                + if i < num1.len() { num1[i] - b'0' } else { 0 }
-                + if i < num2.len() { num2[i] - b'0' } else { 0 };
+        loop {
+            let n1 = num1.next().map(|u| u - b'0');
+            let n2 = num2.next().map(|u| u - b'0');
+            if n1.is_none() && n2.is_none() && !carry {
+                break;
+            }
+            let d = if carry { 1 } else { 0 } + n1.unwrap_or_default() + n2.unwrap_or_default();
             carry = d > 9;
-            v.push(d % 10);
+            v.push((b'0' + d % 10) as char);
         }
-        while v.len() > 1 && v.last() == Some(&0) {
-            v.pop();
-        }
-        v.iter().rev().map(|u| (u + b'0') as char).collect()
+        v.iter().rev().collect()
     }
 }
 
