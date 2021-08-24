711028626506855c02d8a6d1493d107d61de5fcb
sugyan <sugi1982@gmail.com>
2021-08-24 22:39:11 +0900

    Update august-leetcoding-challenge-2021/week-4/3917

--- others/august-leetcoding-challenge-2021/week-4/3917/lib.rs
+++ others/august-leetcoding-challenge-2021/week-4/3917/lib.rs
@@ -2,20 +2,19 @@ pub struct Solution;
 
 impl Solution {
     pub fn complex_number_multiply(num1: String, num2: String) -> String {
-        let n1 = num1
-            .trim_end_matches('i')
-            .split('+')
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect::<Vec<_>>();
-        let n2 = num2
-            .trim_end_matches('i')
-            .split('+')
-            .filter_map(|s| s.parse::<i32>().ok())
+        let n = [num1, num2]
+            .iter()
+            .map(|s| {
+                s.trim_end_matches('i')
+                    .split('+')
+                    .filter_map(|s| s.parse::<i32>().ok())
+                    .collect::<Vec<_>>()
+            })
             .collect::<Vec<_>>();
         format!(
             "{}+{}i",
-            n1[0] * n2[0] - n1[1] * n2[1],
-            n1[0] * n2[1] + n1[1] * n2[0]
+            n[0][0] * n[1][0] - n[0][1] * n[1][1],
+            n[0][0] * n[1][1] + n[0][1] * n[1][0]
         )
     }
 }
