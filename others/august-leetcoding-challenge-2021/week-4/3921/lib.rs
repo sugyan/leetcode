bbf730fcf117903c645e42aa18144b6a4277a3eb
sugyan <sugi1982@gmail.com>
2021-08-27 23:53:25 +0900

    Update august-leetcoding-challenge-2021/week-4/3921

--- others/august-leetcoding-challenge-2021/week-4/3921/lib.rs
+++ others/august-leetcoding-challenge-2021/week-4/3921/lib.rs
@@ -2,32 +2,19 @@ pub struct Solution;
 
 impl Solution {
     pub fn find_lu_slength(strs: Vec<String>) -> i32 {
-        let is_subsequence = |s1: &str, s2: &str| -> bool {
-            let mut s2 = s2.chars();
-            for c1 in s1.chars() {
-                loop {
-                    if let Some(c2) = s2.next() {
-                        if c2 == c1 {
-                            break;
-                        }
-                    } else {
-                        return false;
-                    }
-                }
-            }
-            true
-        };
         let mut strs = strs;
         strs.sort_unstable_by_key(|s| s.len());
-        for (i, s) in strs.iter().enumerate().rev() {
-            if (0..strs.len())
-                .filter(|&j| i != j)
-                .all(|j| !is_subsequence(s, &strs[j]))
-            {
-                return s.len() as i32;
-            }
+        if let Some(i) = (0..strs.len()).rfind(|&i| {
+            (0..strs.len()).all(|j| j == i || !Self::is_subsequence(&strs[i], &strs[j]))
+        }) {
+            strs[i].len() as i32
+        } else {
+            -1
         }
-        -1
+    }
+    fn is_subsequence(s1: &str, s2: &str) -> bool {
+        let mut s2 = s2.chars();
+        s1.chars().all(|c1| s2.any(|c2| c2 == c1))
     }
 }
 
