76da1375302b90bf5da10a9a894f8f0a9a29b457
sugyan <sugi1982@gmail.com>
2021-09-21 17:19:28 +0900

    Update september-leetcoding-challenge-2021/week-3/3982

--- others/september-leetcoding-challenge-2021/week-3/3982/lib.rs
+++ others/september-leetcoding-challenge-2021/week-3/3982/lib.rs
@@ -3,9 +3,12 @@ pub struct Solution;
 impl Solution {
     pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
         nums.iter()
-            .fold((0, 0), |acc, &x| {
-                let len = if x == 0 { 0 } else { acc.1 + 1 };
-                (acc.0.max(len), len)
+            .fold((0, 0), |(answer, len), &x| {
+                if x == 0 {
+                    (answer, 0)
+                } else {
+                    (answer.max(len + 1), len + 1)
+                }
             })
             .0
     }
