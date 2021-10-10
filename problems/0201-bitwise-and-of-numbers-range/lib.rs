19a1c028c64abc9eb57d61d3dd9e268b3485612c
sugyan <sugi1982@gmail.com>
2021-10-10 20:54:05 +0900

    Update 201. Bitwise AND of Numbers Range
    https://leetcode.com/problems/bitwise-and-of-numbers-range/

--- problems/0201-bitwise-and-of-numbers-range/lib.rs
+++ problems/0201-bitwise-and-of-numbers-range/lib.rs
@@ -2,16 +2,11 @@ pub struct Solution {}
 
 impl Solution {
     pub fn range_bitwise_and(m: i32, n: i32) -> i32 {
-        let mut answer = 0;
-        let mut mask = 0x4000_0000;
-        for _ in 0..32 {
-            if (m & mask) != (n & mask) {
-                break;
-            }
-            answer += m & mask;
-            mask >>= 1;
+        let mut n = n;
+        while n > m {
+            n &= n - 1;
         }
-        answer
+        n
     }
 }
 
@@ -26,6 +21,11 @@ mod tests {
 
     #[test]
     fn example_2() {
-        assert_eq!(0, Solution::range_bitwise_and(0, 1));
+        assert_eq!(0, Solution::range_bitwise_and(0, 0));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(0, Solution::range_bitwise_and(1, 2147483647));
     }
 }
