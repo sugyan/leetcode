c950ba6650a5a5f9e68d1b505233e24b7dbb2e15
sugyan <sugi1982@gmail.com>
2021-10-12 17:33:34 +0900

    Update 374. Guess Number Higher or Lower

--- problems/0374-guess-number-higher-or-lower/lib.rs
+++ problems/0374-guess-number-higher-or-lower/lib.rs
@@ -1,16 +1,21 @@
+use std::cmp::Ordering;
+
 pub struct Solution;
 
-/**
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is lower than the guess number
- *			      1 if num is higher than the guess number
- *               otherwise return 0
- * unsafe fn guess(num: i32) -> i32 {}
- */
+static mut NUMBER: i32 = 1;
+
+unsafe fn guess(num: i32) -> i32 {
+    match NUMBER.cmp(&num) {
+        Ordering::Less => -1,
+        Ordering::Equal => 0,
+        Ordering::Greater => 1,
+    }
+}
 
 impl Solution {
-    unsafe fn guessNumber(n: i32) -> i32 {
+    /// # Safety
+    /// ?
+    pub unsafe fn guess_number(n: i32) -> i32 {
         let (mut lo, mut hi) = (1, n);
         while lo < hi {
             let m = lo + (hi - lo) / 2;
@@ -21,5 +26,42 @@ impl Solution {
             }
         }
         lo
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        unsafe {
+            NUMBER = 6;
+            assert_eq!(6, Solution::guess_number(10));
+        }
+    }
+
+    #[test]
+    fn example_2() {
+        unsafe {
+            NUMBER = 1;
+            assert_eq!(1, Solution::guess_number(1));
+        }
+    }
+
+    #[test]
+    fn example_3() {
+        unsafe {
+            NUMBER = 1;
+            assert_eq!(1, Solution::guess_number(2));
+        }
+    }
+
+    #[test]
+    fn example_4() {
+        unsafe {
+            NUMBER = 2;
+            assert_eq!(2, Solution::guess_number(2));
+        }
     }
 }
