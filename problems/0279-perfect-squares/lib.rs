9f0eaa063e09b7ab9f8e1144aa9acc8e6e1914f2
sugyan <sugi1982@gmail.com>
2021-10-14 10:06:38 +0900

    Update 279. Perfect Squares
    https://leetcode.com/problems/perfect-squares/

--- problems/0279-perfect-squares/lib.rs
+++ problems/0279-perfect-squares/lib.rs
@@ -1,17 +1,13 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn num_squares(n: i32) -> i32 {
-        let mut dp: Vec<i32> = vec![0; n as usize + 1];
+        let mut dp = vec![i32::MAX; n as usize + 1];
+        dp[0] = 0;
         for i in 1..=n as usize {
-            let mut min = std::i32::MAX;
-            for j in 1.. {
-                if j * j > i {
-                    break;
-                }
-                min = std::cmp::min(min, dp[i - j * j] + 1);
+            for j in (1..).take_while(|j| j * j <= i) {
+                dp[i] = dp[i].min(dp[i - j * j] + 1);
             }
-            dp[i] = min;
         }
         dp[n as usize]
     }
