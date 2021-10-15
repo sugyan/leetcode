866fb070ac0c401f347cc9c3f8d76f9cee34a44a
sugyan <sugi1982@gmail.com>
2021-10-15 10:32:39 +0900

    Update 309. Best Time to Buy and Sell Stock with Cooldown
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

--- problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/lib.rs
+++ problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/lib.rs
@@ -1,21 +1,18 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        if prices.len() < 2 {
-            return 0;
-        }
-        let mut buy: Vec<i32> = vec![0; prices.len()];
-        let mut sell: Vec<i32> = vec![0; prices.len()];
-        let mut cooldown: Vec<i32> = vec![0; prices.len()];
-        sell[0] = std::i32::MIN;
+        let mut buy = vec![0; prices.len()];
+        let mut sell = vec![0; prices.len()];
+        let mut cooldown = vec![0; prices.len()];
         buy[0] = -prices[0];
+        sell[0] = i32::MIN;
         for i in 1..prices.len() {
-            buy[i] = std::cmp::max(buy[i - 1], cooldown[i - 1] - prices[i]);
-            sell[i] = std::cmp::max(sell[i - 1], buy[i - 1] + prices[i]);
-            cooldown[i] = std::cmp::max(cooldown[i - 1], sell[i - 1]);
+            buy[i] = buy[i - 1].max(cooldown[i - 1] - prices[i]);
+            sell[i] = sell[i - 1].max(buy[i - 1] + prices[i]);
+            cooldown[i] = cooldown[i - 1].max(sell[i - 1]);
         }
-        std::cmp::max(sell[prices.len() - 1], cooldown[prices.len() - 1])
+        sell[prices.len() - 1].max(cooldown[prices.len() - 1])
     }
 }
 
@@ -26,5 +23,10 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(3, Solution::max_profit(vec![1, 2, 3, 0, 2]));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(0, Solution::max_profit(vec![1]));
     }
 }
