b2d830a6b33ab9d145be5ef31da4a2da499f6ed6
sugyan <sugi1982@gmail.com>
2021-10-16 21:16:21 +0900

    Add 123. Best Time to Buy and Sell Stock III
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/

---
+++ problems/0123-best-time-to-buy-and-sell-stock-iii/lib.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut dp = [0; 3];
+        let mut min = [prices[0]; 3];
+        for price in prices.iter().skip(1) {
+            for i in 1..3 {
+                min[i] = min[i].min(price - dp[i - 1]);
+                dp[i] = dp[i].max(price - min[i]);
+            }
+        }
+        dp[2]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(6, Solution::max_profit(vec![3, 3, 5, 0, 0, 3, 1, 4]));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(4, Solution::max_profit(vec![1, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(0, Solution::max_profit(vec![7, 6, 4, 3, 1]));
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(0, Solution::max_profit(vec![1]));
+    }
+}
