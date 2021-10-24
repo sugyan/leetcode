01ddb66a7c4bf57a75903ee0b1b4eb2d2785e7bb
sugyan <sugi1982@gmail.com>
2021-10-24 11:18:12 +0900

    Update 222. Count Complete Tree Nodes
    https://leetcode.com/problems/count-complete-tree-nodes/

--- problems/0222-count-complete-tree-nodes/lib.rs
+++ problems/0222-count-complete-tree-nodes/lib.rs
@@ -2,35 +2,35 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use utils::TreeNode;
 
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if let Some(r) = root {
-            let ld = {
-                let mut h = 1;
-                let mut node = r.borrow().left.clone();
-                while let Some(n) = node {
-                    h += 1;
-                    node = n.borrow().left.clone();
-                }
-                h
-            };
-            let rd = {
-                let mut h = 1;
-                let mut node = r.borrow().right.clone();
-                while let Some(n) = node {
-                    h += 1;
-                    node = n.borrow().right.clone();
-                }
-                h
-            };
+        Self::helper(&root)
+    }
+    fn helper(node: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(n) = node {
+            let ld = Self::dfs_l(&node);
+            let rd = Self::dfs_r(&node);
             if ld == rd {
-                2i32.pow(ld) - 1
+                2_i32.pow(ld) - 1
             } else {
-                1 + Solution::count_nodes(r.borrow().left.clone())
-                    + Solution::count_nodes(r.borrow().right.clone())
+                1 + Self::helper(&n.borrow().left) + Self::helper(&n.borrow().right)
             }
+        } else {
+            0
+        }
+    }
+    fn dfs_l(node: &Option<Rc<RefCell<TreeNode>>>) -> u32 {
+        if let Some(n) = node {
+            Self::dfs_l(&n.borrow().left) + 1
+        } else {
+            0
+        }
+    }
+    fn dfs_r(node: &Option<Rc<RefCell<TreeNode>>>) -> u32 {
+        if let Some(n) = node {
+            Self::dfs_r(&n.borrow().right) + 1
         } else {
             0
         }
@@ -55,5 +55,15 @@ mod tests {
                 Some(6)
             ]))
         );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(0, Solution::count_nodes(None));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(1, Solution::count_nodes(to_tree(vec![Some(1)])));
     }
 }
