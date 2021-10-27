4052ef553f4b1c735428180b01f3d14d64c2618d
sugyan <sugi1982@gmail.com>
2021-10-27 10:30:28 +0900

    Update 75. Sort Colors
    https://leetcode.com/problems/sort-colors/

--- problems/0075-sort-colors/lib.rs
+++ problems/0075-sort-colors/lib.rs
@@ -2,23 +2,24 @@ pub struct Solution {}
 
 impl Solution {
     pub fn sort_colors(nums: &mut Vec<i32>) {
-        let (mut l, mut r) = (0, nums.len() - 1);
+        let (mut lo, mut hi) = (0, nums.len() - 1);
         let mut i = 0;
-        while i <= r {
+        while i <= hi {
             match nums[i] {
                 0 => {
-                    nums.swap(l, i);
-                    l += 1;
+                    nums.swap(lo, i);
+                    lo += 1;
                     i += 1;
                 }
+                1 => i += 1,
                 2 => {
-                    nums.swap(r, i);
-                    if r == 0 {
+                    nums.swap(hi, i);
+                    if hi == 0 {
                         break;
                     }
-                    r -= 1;
+                    hi -= 1;
                 }
-                _ => i += 1,
+                _ => unreachable!(),
             }
         }
     }
@@ -33,5 +34,26 @@ mod tests {
         let mut nums = vec![2, 0, 2, 1, 1, 0];
         Solution::sort_colors(&mut nums);
         assert_eq!(vec![0, 0, 1, 1, 2, 2], nums);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut nums = vec![2, 0, 1];
+        Solution::sort_colors(&mut nums);
+        assert_eq!(vec![0, 1, 2], nums);
+    }
+
+    #[test]
+    fn example_3() {
+        let mut nums = vec![0];
+        Solution::sort_colors(&mut nums);
+        assert_eq!(vec![0], nums);
+    }
+
+    #[test]
+    fn example_4() {
+        let mut nums = vec![1];
+        Solution::sort_colors(&mut nums);
+        assert_eq!(vec![1], nums);
     }
 }
