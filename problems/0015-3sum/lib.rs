e6c2bc1e7e84a69c6a75cb65abf8858551fc5870
sugyan <sugi1982@gmail.com>
2021-10-28 10:53:35 +0900

    Update 15. 3Sum
    https://leetcode.com/problems/3sum/

--- problems/0015-3sum/lib.rs
+++ problems/0015-3sum/lib.rs
@@ -1,40 +1,37 @@
-pub struct Solution {}
+use std::cmp::Ordering;
+
+pub struct Solution;
 
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 3 {
+            return Vec::new();
+        }
         let mut nums = nums;
-        nums.sort_unstable();
+        nums.sort();
         let mut answer = Vec::new();
-        let mut prev = None;
-        for i in 0..nums.len() {
-            if let Some(p) = prev {
-                if p == nums[i] {
-                    continue;
-                }
+        for i in 0..nums.len() - 2 {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
             }
-            let (mut l, mut r) = (i + 1, nums.len() - 1);
-            while l < r {
-                match nums[i].cmp(&-(nums[l] + nums[r])) {
-                    std::cmp::Ordering::Less => {
-                        l += 1;
-                    }
-                    std::cmp::Ordering::Greater => {
-                        r -= 1;
-                    }
-                    std::cmp::Ordering::Equal => {
-                        answer.push(vec![nums[i], nums[l], nums[r]]);
-                        while l < r && nums[l + 1] == nums[l] {
-                            l += 1;
+            let (mut j, mut k) = (i + 1, nums.len() - 1);
+            while j < k {
+                match (nums[i] + nums[j] + nums[k]).cmp(&0) {
+                    Ordering::Less => j += 1,
+                    Ordering::Greater => k -= 1,
+                    Ordering::Equal => {
+                        answer.push(vec![nums[i], nums[j], nums[k]]);
+                        while j < k && nums[j] == nums[j + 1] {
+                            j += 1;
                         }
-                        while l < r && nums[r - 1] == nums[r] {
-                            r -= 1;
+                        while j < k && nums[k] == nums[k - 1] {
+                            k -= 1;
                         }
-                        l += 1;
-                        r -= 1;
+                        j += 1;
+                        k -= 1;
                     }
                 }
             }
-            prev = Some(nums[i]);
         }
         answer
     }
@@ -47,7 +44,18 @@ mod tests {
     #[test]
     fn example_1() {
         let mut ret = Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]);
+        ret.iter_mut().for_each(|v| v.sort());
         ret.sort();
         assert_eq!(vec![vec![-1, -1, 2], vec![-1, 0, 1]], ret);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(Vec::<Vec<i32>>::new(), Solution::three_sum(vec![]));
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(Vec::<Vec<i32>>::new(), Solution::three_sum(vec![0]));
     }
 }
