99cec16adce5a4e3a2d6580cff84ac86dd8f7ba6
sugyan <sugi1982@gmail.com>
2021-10-30 22:52:42 +0900

    Update 1044. Longest Duplicate Substring

--- problems/1044-longest-duplicate-substring/lib.rs
+++ problems/1044-longest-duplicate-substring/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-const P: i64 = 31;
+const P: i64 = 127;
 const MOD: i64 = 1_000_000_007;
 
 pub struct Solution;
@@ -9,21 +9,17 @@ impl Solution {
     pub fn longest_dup_substring(s: String) -> String {
         // prepare
         let mut v = vec![0; s.len() + 1];
-        let mut inv = vec![1; s.len() + 1];
+        let mut pows = vec![1; s.len() + 1];
         for (i, u) in s.bytes().enumerate() {
-            inv[i + 1] = inv[i] * P % MOD;
-            v[i + 1] = (v[i] + inv[i] * u as i64) % MOD;
-        }
-        inv[s.len()] = Self::pow(inv[s.len()], MOD - 2);
-        for i in (0..s.len()).rev() {
-            inv[i] = inv[i + 1] * P % MOD;
+            v[i + 1] = (v[i] * P + u as i64) % MOD;
+            pows[i + 1] = pows[i] * P % MOD;
         }
         // binary search
         let mut answer = 0;
         let (mut lo, mut hi) = (0, s.len());
         while lo < hi {
             let mid = (lo + hi) / 2;
-            if let Some(i) = Self::search(&s, &v, &inv, mid) {
+            if let Some(i) = Self::search(&s, &v, pows[mid], mid) {
                 answer = i;
                 lo = mid + 1;
             } else {
@@ -32,10 +28,10 @@ impl Solution {
         }
         s[answer..answer + lo - 1].to_string()
     }
-    fn search(s: &str, v: &[i64], inv: &[i64], len: usize) -> Option<usize> {
+    fn search(s: &str, v: &[i64], pow: i64, len: usize) -> Option<usize> {
         let mut hm = HashMap::new();
-        for i in 0..=v.len() - 1 - len {
-            let hash = ((v[i + len] - v[i]).rem_euclid(MOD) * inv[i]) % MOD;
+        for i in 0..=s.len() - len {
+            let hash = (v[i + len] - v[i] * pow).rem_euclid(MOD);
             if let Some(&j) = hm.get(&hash) {
                 if s[i..i + len] == s[j..j + len] {
                     return Some(i);
@@ -44,19 +40,6 @@ impl Solution {
             hm.insert(hash, i);
         }
         None
-    }
-    fn pow(x: i64, y: i64) -> i64 {
-        let mut ret = 1;
-        let mut x = x;
-        let mut y = y;
-        while y > 0 {
-            if y & 1 > 0 {
-                ret = ret * x % MOD;
-            }
-            x = x * x % MOD;
-            y >>= 1;
-        }
-        ret
     }
 }
 
