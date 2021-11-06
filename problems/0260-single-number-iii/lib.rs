2873334a3f88fca12ec1614e388b5cbd684d8eda
sugyan <sugi1982@gmail.com>
2021-11-06 15:19:40 +0900

    Update 260. Single Number III
    https://leetcode.com/problems/single-number-iii/

--- problems/0260-single-number-iii/lib.rs
+++ problems/0260-single-number-iii/lib.rs
@@ -1,15 +1,15 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
-        let xor = nums.iter().fold(0, |acc, x| acc ^ x);
-        let r = xor & !(xor - 1);
+        let xor = nums.iter().fold(0, |acc, x| acc ^ x) as i64;
+        let lsb = (xor & -xor) as i32;
         let mut answer = vec![0, 0];
-        for n in nums.iter() {
-            if n & r == 0 {
-                answer[0] ^= *n;
+        for &n in &nums {
+            if n & lsb == 0 {
+                answer[0] ^= n;
             } else {
-                answer[1] ^= *n;
+                answer[1] ^= n;
             }
         }
         answer
@@ -23,7 +23,21 @@ mod tests {
     #[test]
     fn example_1() {
         let mut ret = Solution::single_number(vec![1, 2, 1, 3, 2, 5]);
-        ret.sort_unstable();
+        ret.sort();
         assert_eq!(vec![3, 5], ret);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut ret = Solution::single_number(vec![-1, 0]);
+        ret.sort();
+        assert_eq!(vec![-1, 0], ret);
+    }
+
+    #[test]
+    fn example_3() {
+        let mut ret = Solution::single_number(vec![0, 1]);
+        ret.sort();
+        assert_eq!(vec![0, 1], ret);
     }
 }
