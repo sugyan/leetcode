d948e6af009b43df38b0b0f1f50ead2b9e98aeae
sugyan <sugi1982@gmail.com>
2021-11-09 14:54:26 +0900

    Improve 1178. Number of Valid Words for Each Puzzle (216ms -> 28ms)

--- problems/1178-number-of-valid-words-for-each-puzzle/lib.rs
+++ problems/1178-number-of-valid-words-for-each-puzzle/lib.rs
@@ -1,30 +1,26 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
     pub fn find_num_of_valid_words(words: Vec<String>, puzzles: Vec<String>) -> Vec<i32> {
-        let v = words
-            .iter()
-            .map(|w| w.bytes().fold(0_u32, |acc, u| acc | 1 << (u - b'a')))
-            .collect::<Vec<_>>();
-        let groups = v
-            .iter()
-            .enumerate()
-            .fold(vec![Vec::new(); 26], |mut acc, (i, u)| {
-                (0..26).for_each(|j| {
-                    if u & (1 << j) != 0 {
-                        acc[j].push(i);
-                    }
-                });
-                acc
-            });
+        let mut hm = HashMap::new();
+        for w in &words {
+            *hm.entry(w.bytes().fold(0_u32, |acc, u| acc | 1 << (u - b'a')))
+                .or_insert(0) += 1;
+        }
         puzzles
             .iter()
             .map(|p| {
-                let mask = p.bytes().fold(0_u32, |acc, u| acc | 1 << (u - b'a'));
-                groups[(p.as_bytes()[0] - b'a') as usize]
-                    .iter()
-                    .filter(|&i| v[*i] & mask == v[*i])
-                    .count() as i32
+                let first = 1 << (p.as_bytes()[0] - b'a');
+                let mask = p.bytes().skip(1).fold(0, |acc, u| acc | 1 << (u - b'a'));
+                let mut count = *hm.get(&first).unwrap_or(&0);
+                let mut submask = mask;
+                while submask > 0 {
+                    count += hm.get(&(submask | first)).unwrap_or(&0);
+                    submask = (submask - 1) & mask;
+                }
+                count
             })
             .collect()
     }
