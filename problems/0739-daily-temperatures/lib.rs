a4a9b6bc6ce54af1224c32b7977bb2326ef4dea3
sugyan <sugi1982@gmail.com>
2021-11-13 12:19:00 +0900

    Add 739. Daily Temperatures
    https://leetcode.com/problems/daily-temperatures/

---
+++ problems/0739-daily-temperatures/lib.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut answer = vec![0; temperatures.len()];
+        let mut index = vec![None; 101];
+        for (i, &t) in temperatures.iter().enumerate().rev() {
+            if let Some(min) = (t as usize + 1..=100).filter_map(|j| index[j]).min() {
+                answer[i] = (min - i) as i32;
+            }
+            index[t as usize] = Some(i);
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(
+            vec![1, 1, 4, 2, 1, 1, 0, 0],
+            Solution::daily_temperatures(vec![73, 74, 75, 71, 69, 72, 76, 73])
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            vec![1, 1, 1, 0],
+            Solution::daily_temperatures(vec![30, 40, 50, 60])
+        );
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(
+            vec![1, 1, 0],
+            Solution::daily_temperatures(vec![30, 60, 90])
+        );
+    }
+}
