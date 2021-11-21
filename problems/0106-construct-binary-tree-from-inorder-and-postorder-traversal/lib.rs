35c64fa4bcf3f2c43bce9b317f7e04b5d3658759
sugyan <sugi1982@gmail.com>
2021-11-21 14:19:37 +0900

    Update 106. Construct Binary Tree from Inorder and Postorder Traversal
    https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/

--- problems/0106-construct-binary-tree-from-inorder-and-postorder-traversal/lib.rs
+++ problems/0106-construct-binary-tree-from-inorder-and-postorder-traversal/lib.rs
@@ -2,21 +2,21 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use utils::TreeNode;
 
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn build_tree(inorder: Vec<i32>, postorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        Solution::helper(&inorder, &postorder)
+        Self::helper(&inorder, &postorder)
     }
     fn helper(inorder: &[i32], postorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
-        if let Some(last) = postorder.last() {
-            let root = Rc::new(RefCell::new(TreeNode::new(*last)));
-            if let Some(idx) = inorder.iter().position(|&v| v == *last) {
-                root.borrow_mut().left = Solution::helper(&inorder[0..idx], &postorder[0..idx]);
-                root.borrow_mut().right =
-                    Solution::helper(&inorder[idx + 1..], &postorder[idx..postorder.len() - 1]);
+        if let Some(&last) = postorder.last() {
+            if let Some(pos) = inorder.iter().position(|&x| x == last) {
+                return Some(Rc::new(RefCell::new(TreeNode {
+                    val: last,
+                    left: Self::helper(&inorder[0..pos], &postorder[0..pos]),
+                    right: Self::helper(&inorder[pos + 1..], &postorder[pos..postorder.len() - 1]),
+                })));
             }
-            return Some(root);
         }
         None
     }
@@ -40,6 +40,14 @@ mod tests {
                 Some(7)
             ]),
             Solution::build_tree(vec![9, 3, 15, 20, 7], vec![9, 15, 7, 20, 3])
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            to_tree(vec![Some(-1)]),
+            Solution::build_tree(vec![-1], vec![-1])
         );
     }
 }
