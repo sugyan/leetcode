5334cdcc7ae7b4007e871d01eb783f0107fd9dde
sugyan <sugi1982@gmail.com>
2021-11-27 14:21:13 +0900

    Update 238. Product of Array Except Self
    https://leetcode.com/problems/product-of-array-except-self/

--- problems/0238-product-of-array-except-self/lib.rs
+++ problems/0238-product-of-array-except-self/lib.rs
@@ -2,16 +2,13 @@ pub struct Solution {}
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut answer: Vec<i32> = vec![1; nums.len()];
-        let mut n = 1;
-        for i in 0..nums.len() {
-            answer[i] *= n;
-            n *= nums[i];
-        }
-        n = 1;
-        for i in (0..nums.len()).rev() {
-            answer[i] *= n;
-            n *= nums[i];
+        let mut answer = vec![1; nums.len()];
+        let (mut lo, mut hi) = (1, 1);
+        for i in 1..nums.len() {
+            lo *= nums[i - 1];
+            answer[i] *= lo;
+            hi *= nums[nums.len() - i];
+            answer[nums.len() - i - 1] *= hi;
         }
         answer
     }
@@ -26,6 +23,14 @@ mod tests {
         assert_eq!(
             vec![24, 12, 8, 6],
             Solution::product_except_self(vec![1, 2, 3, 4])
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            vec![0, 0, 9, 0, 0],
+            Solution::product_except_self(vec![-1, 1, 0, -3, 3])
         );
     }
 }
