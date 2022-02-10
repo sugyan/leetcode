7fb1deab0d9aa5c6877ee2d5175e80d54bc3d2d5
sugyan <sugi1982@gmail.com>
2022-02-10 13:46:48 +0900

    Add 560. Subarray Sum Equals K
    https://leetcode.com/problems/subarray-sum-equals-k/

---
+++ problems/0560-subarray-sum-equals-k/lib.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut hm = vec![(0, 1)].into_iter().collect::<HashMap<_, _>>();
+        let (mut sum, mut answer) = (0, 0);
+        for num in &nums {
+            sum += num;
+            answer += hm.get(&(sum - k)).unwrap_or(&0);
+            *hm.entry(sum).or_insert(0) += 1;
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(2, Solution::subarray_sum(vec![1, 1, 1], 2));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(2, Solution::subarray_sum(vec![1, 2, 3], 3));
+    }
+}
