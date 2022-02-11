0898ef67af6fa7b557ceb5d667ebba3a03e50e98
sugyan <sugi1982@gmail.com>
2022-02-11 13:54:44 +0900

    Update rust toolchain

--- problems/0560-subarray-sum-equals-k/lib.rs
+++ problems/0560-subarray-sum-equals-k/lib.rs
@@ -4,7 +4,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
-        let mut hm = vec![(0, 1)].into_iter().collect::<HashMap<_, _>>();
+        let mut hm = HashMap::from([(0, 1)]);
         let (mut sum, mut answer) = (0, 0);
         for num in &nums {
             sum += num;
