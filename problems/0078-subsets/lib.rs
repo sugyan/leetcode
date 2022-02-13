d053650a3c6a3238bd7017c1ac96b49bfdffd6b3
sugyan <sugi1982@gmail.com>
2022-02-13 14:02:14 +0900

    Update 78. Subsets
    https://leetcode.com/problems/subsets/

--- problems/0078-subsets/lib.rs
+++ problems/0078-subsets/lib.rs
@@ -1,20 +1,20 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut answer = Vec::new();
-        answer.push(vec![]);
-        for num in nums {
-            for i in 0..answer.len() {
-                let mut v = answer[i].clone();
-                v.push(num);
-                answer.push(v);
-            }
-        }
+        Self::backtrack(&nums, &mut Vec::new(), &mut answer);
         answer
     }
+    fn backtrack(nums: &[i32], v: &mut Vec<i32>, answer: &mut Vec<Vec<i32>>) {
+        answer.push(v.clone());
+        for (i, &num) in nums.iter().enumerate() {
+            v.push(num);
+            Self::backtrack(&nums[i + 1..], v, answer);
+            v.pop();
+        }
+    }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,7 +22,7 @@ mod tests {
     #[test]
     fn example_1() {
         let mut ret = Solution::subsets(vec![1, 2, 3]);
-        for v in ret.iter_mut() {
+        for v in &mut ret {
             v.sort_unstable();
         }
         ret.sort();
@@ -39,5 +39,15 @@ mod tests {
             ],
             ret
         );
+    }
+
+    #[test]
+    fn example_2() {
+        let mut ret = Solution::subsets(vec![0]);
+        for v in &mut ret {
+            v.sort_unstable();
+        }
+        ret.sort();
+        assert_eq!(vec![vec![], vec![0]], ret);
     }
 }
