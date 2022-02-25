1b1a052c04a4d624f85dd9a972d99a1bd7d236f7
sugyan <sugi1982@gmail.com>
2022-02-25 09:29:41 +0900

    Update 165. Compare Version Numbers
    https://leetcode.com/problems/compare-version-numbers/

--- problems/0165-compare-version-numbers/lib.rs
+++ problems/0165-compare-version-numbers/lib.rs
@@ -1,20 +1,25 @@
-pub struct Solution {}
+use std::cmp::Ordering;
+
+pub struct Solution;
 
 impl Solution {
     pub fn compare_version(version1: String, version2: String) -> i32 {
-        let mut v1: Vec<u32> = version1.split('.').map(|s| s.parse().unwrap()).collect();
-        let mut v2: Vec<u32> = version2.split('.').map(|s| s.parse().unwrap()).collect();
-        while v1.len() < v2.len() {
-            v1.push(0);
+        let v1 = version1
+            .split('.')
+            .filter_map(|s| s.parse().ok())
+            .collect::<Vec<_>>();
+        let v2 = version2
+            .split('.')
+            .filter_map(|s| s.parse().ok())
+            .collect::<Vec<_>>();
+        for i in 0..v1.len().max(v2.len()) {
+            match v1.get(i).unwrap_or(&0).cmp(v2.get(i).unwrap_or(&0)) {
+                Ordering::Less => return -1,
+                Ordering::Equal => {}
+                Ordering::Greater => return 1,
+            }
         }
-        while v2.len() < v1.len() {
-            v2.push(0);
-        }
-        match v1.cmp(&v2) {
-            std::cmp::Ordering::Equal => 0,
-            std::cmp::Ordering::Less => -1,
-            std::cmp::Ordering::Greater => 1,
-        }
+        0
     }
 }
 
@@ -25,40 +30,24 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(
-            -1,
-            Solution::compare_version("0.1".to_string(), "1.1".to_string())
-        )
+            0,
+            Solution::compare_version(String::from("1.01"), String::from("1.001"))
+        );
     }
 
     #[test]
     fn example_2() {
         assert_eq!(
-            1,
-            Solution::compare_version("1.0.1".to_string(), "1".to_string())
-        )
+            0,
+            Solution::compare_version(String::from("1.0"), String::from("1.0.0"))
+        );
     }
 
     #[test]
     fn example_3() {
         assert_eq!(
             -1,
-            Solution::compare_version("7.5.2.4".to_string(), "7.5.3".to_string())
-        )
-    }
-
-    #[test]
-    fn example_4() {
-        assert_eq!(
-            0,
-            Solution::compare_version("1.01".to_string(), "1.001".to_string())
-        )
-    }
-
-    #[test]
-    fn example_5() {
-        assert_eq!(
-            0,
-            Solution::compare_version("1.0".to_string(), "1.0.0".to_string())
-        )
+            Solution::compare_version(String::from("0.1"), String::from("1.1"))
+        );
     }
 }
