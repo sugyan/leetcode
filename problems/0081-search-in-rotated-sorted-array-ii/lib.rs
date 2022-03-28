7acbc4498862cce7a3a302512f831a0cad043e8d
sugyan <sugi1982@gmail.com>
2022-03-28 10:10:53 +0900

    Update 81. Search in Rotated Sorted Array II
    https://leetcode.com/problems/search-in-rotated-sorted-array-ii/

--- problems/0081-search-in-rotated-sorted-array-ii/lib.rs
+++ problems/0081-search-in-rotated-sorted-array-ii/lib.rs
@@ -1,43 +1,31 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> bool {
         if nums.is_empty() {
             return false;
         }
-        let (mut l, mut r) = (0, nums.len() - 1);
-        while l <= r {
-            let m = l + (r - l) / 2;
-            let n = nums[m];
-            if n == target {
+        let (mut lo, mut hi) = (0, nums.len() - 1);
+        while lo <= hi {
+            let mid = (lo + hi) / 2;
+            if nums[mid] == target {
                 return true;
             }
-            match n.cmp(&nums[r]) {
-                std::cmp::Ordering::Greater => {
-                    if target > n || target <= nums[r] {
-                        l = m + 1;
-                    } else if m > 0 {
-                        r = m - 1;
-                    } else {
-                        break;
-                    }
+            if nums[lo] == nums[mid] && nums[mid] == nums[hi] {
+                lo += 1;
+                hi -= hi.min(1);
+                continue;
+            }
+            if nums[lo] <= nums[mid] {
+                if nums[lo] <= target && nums[mid] > target {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
                 }
-                std::cmp::Ordering::Equal => {
-                    if r > 0 {
-                        r -= 1;
-                    } else {
-                        break;
-                    }
-                }
-                std::cmp::Ordering::Less => {
-                    if target <= nums[r] && target > n {
-                        l = m + 1;
-                    } else if m > 0 {
-                        r = m - 1;
-                    } else {
-                        break;
-                    }
-                }
+            } else if nums[mid] < target && nums[hi] >= target {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
             }
         }
         false
@@ -48,11 +36,13 @@ impl Solution {
 mod tests {
     use super::*;
 
+    #[allow(clippy::bool_assert_comparison)]
     #[test]
     fn example_1() {
         assert_eq!(true, Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 0));
     }
 
+    #[allow(clippy::bool_assert_comparison)]
     #[test]
     fn example_2() {
         assert_eq!(false, Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 3));
