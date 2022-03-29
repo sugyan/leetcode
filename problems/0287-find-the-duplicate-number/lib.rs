567fb3edc0f5855cdab79e16fd6dcc4230232d9d
sugyan <sugi1982@gmail.com>
2022-03-29 13:51:21 +0900

    Update 287. Find the Duplicate Number
    https://leetcode.com/problems/find-the-duplicate-number/

--- problems/0287-find-the-duplicate-number/lib.rs
+++ problems/0287-find-the-duplicate-number/lib.rs
@@ -1,19 +1,19 @@
-pub struct Solution {}
+pub struct Solution;
 
 impl Solution {
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
         let mut p = (0, 0);
         loop {
-            p.0 = nums[p.0 as usize];
-            p.1 = nums[p.1 as usize];
-            p.1 = nums[p.1 as usize];
+            p.0 = nums[p.0] as usize;
+            p.1 = nums[p.1] as usize;
+            p.1 = nums[p.1] as usize;
             if p.0 == p.1 {
                 p.0 = 0;
                 while p.0 != p.1 {
-                    p.0 = nums[p.0 as usize];
-                    p.1 = nums[p.1 as usize];
+                    p.0 = nums[p.0] as usize;
+                    p.1 = nums[p.1] as usize;
                 }
-                return p.0;
+                return p.0 as i32;
             }
         }
     }
