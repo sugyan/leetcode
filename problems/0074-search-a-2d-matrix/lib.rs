9b20eb0dd4b13674cec1b563ff579597a8805fa1
sugyan <sugi1982@gmail.com>
2022-03-30 13:58:20 +0900

    Update 74. Search a 2D Matrix
    https://leetcode.com/problems/search-a-2d-matrix/

--- problems/0074-search-a-2d-matrix/lib.rs
+++ problems/0074-search-a-2d-matrix/lib.rs
@@ -5,21 +5,10 @@ impl Solution {
         if matrix.is_empty() || matrix[0].is_empty() {
             return false;
         }
-        let i = if let Some(i) = (1..matrix.len()).find(|i| matrix[*i][0] > target) {
-            i - 1
-        } else {
-            matrix.len() - 1
-        };
-        let (mut l, mut r) = (0, matrix[0].len());
-        while r - l > 1 {
-            let m = l + (r - l) / 2;
-            match matrix[i][m].cmp(&target) {
-                std::cmp::Ordering::Equal => return true,
-                std::cmp::Ordering::Greater => r = m,
-                std::cmp::Ordering::Less => l = m,
-            }
+        match matrix.binary_search_by(|row| row[0].cmp(&target)) {
+            Ok(_) => true,
+            Err(i) => i > 0 && matrix[i - 1].binary_search(&target).is_ok(),
         }
-        matrix[i][l] == target
     }
 }
 
@@ -27,23 +16,35 @@ impl Solution {
 mod tests {
     use super::*;
 
+    #[allow(clippy::bool_assert_comparison)]
+    #[rustfmt::skip]
     #[test]
     fn example_1() {
         assert_eq!(
             true,
             Solution::search_matrix(
-                vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 50]],
+                vec![
+                    vec![ 1,  3,  5,  7],
+                    vec![10, 11, 16, 20],
+                    vec![23, 30, 34, 60]
+                ],
                 3
             )
         );
     }
 
+    #[allow(clippy::bool_assert_comparison)]
+    #[rustfmt::skip]
     #[test]
     fn example_2() {
         assert_eq!(
             false,
             Solution::search_matrix(
-                vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 50]],
+                vec![
+                    vec![ 1,  3,  5,  7],
+                    vec![10, 11, 16, 20],
+                    vec![23, 30, 34, 60]
+                ],
                 13
             )
         );
